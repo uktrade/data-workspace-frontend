88dded36db7ad4a5a213e3be0438af3f062bdbcd
Michal Charemza <michal@charemza.name>
2019-03-15 14:49:16 +0000

    (feat) Store SSO IO in profile

--- app/app/admin.py
+++ app/app/admin.py
@@ -11,6 +11,7 @@ from django.contrib.auth.models import (
 from app.models import (
     Database,
     Privilage,
+    Profile,
 )
 
 admin.site.register(Database)
@@ -32,6 +33,13 @@ class AppUserCreationForm(forms.ModelForm):
         return user
 
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
 class AppUserAdmin(UserAdmin):
     add_form_template = 'admin/change_form.html'
     add_form = AppUserCreationForm
@@ -41,6 +49,12 @@ class AppUserAdmin(UserAdmin):
             'fields': ('email', ),
         }),
     )
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super().get_inline_instances(request, obj)
 
 admin.site.unregister(User)
 admin.site.register(User, AppUserAdmin)
--- app/app/models.py
+++ app/app/models.py
@@ -1,8 +1,26 @@
 import uuid
 
 from django.contrib.auth.models import User
-from django.db import models
 from django.core.validators import RegexValidator
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    sso_id = models.UUIDField(unique=True, default=uuid.uuid4)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Database(models.Model):
