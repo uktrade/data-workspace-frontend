7b229759e8dff6ceb53bc020c390a885dd201cc2
Michal Charemza <michal@charemza.name>
2019-03-26 17:00:32 +0000

    (feat) Management command to create credentials for all users

--- app/app/management/commands/store_db_creds_in_s3.py
+++ app/app/management/commands/store_db_creds_in_s3.py
@@ -1,5 +1,12 @@
+from django.contrib.auth import (
+    get_user_model,
+)
 from django.core.management.base import (
     BaseCommand,
+)
+
+from app.shared import (
+    new_private_database_credentials,
 )
 
 
@@ -8,4 +15,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('store_db_creds_in_s3 started')
+
+        User = get_user_model()
+        all_users = User.objects.order_by('last_name', 'first_name', 'id')
+        for user in all_users:
+            self.stdout.write(f'Creating credentials for {user.email}')
+            new_private_database_credentials(user.email)
+
         self.stdout.write(self.style.SUCCESS('store_db_creds_in_s3 finished'))
