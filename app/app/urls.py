f03cfb04a8e957f02046b216a460522c4eccc089
Mark Higham <mark.higham@gmail.com>
2019-06-17 11:32:43 +0100

    alignment for the tables

--- app/app/urls.py
+++ app/app/urls.py
@@ -109,7 +109,7 @@ urlpatterns = [
          name='dataset_fullpath'),
     path('dataset/<str:dataset_id>', login_required(dataset_item_view), name='dataset_item'),
     path('analysis', login_required(root_view), name='root'),
-    path('', login_required(root_view), name='root'),
+    # path('', login_required(root_view), name='root'),
     path('error_403', public_error_403_html_view),
     path('error_404', public_error_404_html_view),
     path('error_500', public_error_500_html_view),
