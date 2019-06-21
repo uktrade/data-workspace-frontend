45bea03c71ce0908781d7598b98b8941812c6b52
Mark Higham <mark.higham@gmail.com>
2019-06-21 09:42:16 +0100

    removed the last inline_edit reference

--- app/app/context_processors.py
+++ app/app/context_processors.py
@@ -7,6 +7,4 @@ def common(request):
     return {
         'root_href': f'{request.scheme}://{settings.APPLICATION_ROOT_DOMAIN}/',
         'support_url': settings.SUPPORT_URL,
-
-        'inline_edit': True,
     }
