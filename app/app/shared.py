9f18e7b1dfbce0aac5ce99c4fc6c856e84cb1de3
Michal Charemza <michal@charemza.name>
2019-06-21 19:36:13 +0100

    (refactor) Tables in schema used in multiple views

--- app/app/shared.py
+++ app/app/shared.py
@@ -140,3 +140,18 @@ def set_application_stopped(application_instance):
     application_instance.state = 'STOPPED'
     application_instance.single_running_or_spawning_integrity = str(application_instance.id)
     application_instance.save()
+
+
+def tables_in_schema(cur, schema):
+    logger.info('tables_in_schema: %s', schema)
+    cur.execute("""
+        SELECT
+            tablename
+        FROM
+            pg_tables
+        WHERE
+            schemaname = %s
+    """, (schema,))
+    results = [result[0] for result in cur.fetchall()]
+    logger.info('tables_in_schema: %s %s', schema, results)
+    return results
