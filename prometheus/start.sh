119a381dece7144dd4dfb1347cab0d404f3f908c
Michal Charemza <michal@charemza.name>
2019-07-10 11:02:37 +0100

    (feat) Ensure to exit container if any process exits

--- prometheus/start.sh
+++ prometheus/start.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-python3 fetch_targets.py & \
-/usr/local/bin/prometheus \
-    --config.file=/etc/prometheus/prometheus.yml \
-    --storage.tsdb.path=/prometheus \
-    --web.console.libraries=/etc/prometheus/console_libraries \
-    --web.console.templates=/etc/prometheus/consoles \
-    --log.level=debug
+parallel --will-cite --line-buffer --jobs 2 --halt now,done=1 ::: \
+    "python3 fetch_targets.py" \
+    "/usr/local/bin/prometheus \
+        --config.file=/etc/prometheus/prometheus.yml \
+        --storage.tsdb.path=/prometheus \
+        --web.console.libraries=/etc/prometheus/console_libraries \
+        --web.console.templates=/etc/prometheus/consoles \
+        --log.level=debug"
