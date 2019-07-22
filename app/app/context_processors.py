7682adff275e0c7bd8da307e0ff8ded1aa760110
Nick Ross <nick.ir.ross@gmail.com>
2019-07-22 11:54:51 +0100

    Stop setting support url to none

--- app/app/context_processors.py
+++ app/app/context_processors.py
@@ -6,6 +6,6 @@ from django.conf import (
 def common(request):
     return {
         'root_href': f'{request.scheme}://{settings.APPLICATION_ROOT_DOMAIN}/',
-        'support_url': None,
+        'support_url': settings.SUPPORT_URL,
         'google_analytics_site_id': settings.GOOGLE_ANALYTICS_SITE_ID
     }
