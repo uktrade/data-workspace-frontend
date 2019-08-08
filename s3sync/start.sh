daa5d6b0b996e5884b06d7c0f0dd22ad76e11ea3
Michal Charemza <michal@charemza.name>
2019-08-08 14:32:20 +0100

    (feat) S3 Sync with better excludes

--- s3sync/start.sh
+++ s3sync/start.sh
@@ -5,10 +5,16 @@ set -e
 # Path-style even though it's deprecated. The bucket names have dots in, and
 # at the time of writing the host-style certs returned by AWS are wildcards
 # and don't support dots in the bucket name
+# Excluding .checkpoints to not have issues with jupyters3 .checkpoints
+# folders that are subfolders of files in S3
+# Excluding all all dot files locally since they are a) probably unnecessary
+# and b) contains a sqlite database for temporary data
 mobius3 \
     /home/s3sync/data \
     https://s3-${S3_REGION}.amazonaws.com/${S3_BUCKET}/ \
     ${S3_REGION} \
     --prefix ${S3_PREFIX} \
     --log-level INFO \
-    --credentials-source ecs-container-endpoint
+    --credentials-source ecs-container-endpoint \
+    --exclude-remote '.*(/|^)\.checkpoints/.*' \
+    --exclude-local '.*/\..*'
