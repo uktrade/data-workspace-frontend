0b2f22d27e85c390bd24667c1bfe65df1f3957b7
Nick Ross <nick.ir.ross@gmail.com>
2019-08-16 14:24:11 +0100

    Only update table name if it has changed

--- dataworkspace/dataworkspace/apps/datasets/migrations/0004_auto_20190813_1536.py
+++ dataworkspace/dataworkspace/apps/datasets/migrations/0004_auto_20190813_1536.py
@@ -7,15 +7,16 @@ def generate_table_name(apps, _):
     model = apps.get_model('datasets', 'ReferenceDataset')
     for r in model.objects.all():
         r._original_table_name = 'refdata__{}'.format(r.id)
-        r.table_name = 'ref_{}'.format(r.slug.replace('-', '_'))
-        print('Changing table "{}" to "{}"'.format(r._original_table_name, r.table_name))
-        r.schema_version += 1
-        r.save()
-        with connection.schema_editor() as editor:
-            try:
-                editor.alter_db_table(model, r._original_table_name, r.table_name)
-            except ProgrammingError:
-                pass
+        if r.table_name is None or r.table_name == r._original_table_name:
+            r.table_name = 'ref_{}'.format(r.slug.replace('-', '_'))
+            print('Changing table "{}" to "{}"'.format(r._original_table_name, r.table_name))
+            r.schema_version += 1
+            r.save()
+            with connection.schema_editor() as editor:
+                try:
+                    editor.alter_db_table(model, r._original_table_name, r.table_name)
+                except ProgrammingError:
+                    pass
 
 
 class Migration(migrations.Migration):
