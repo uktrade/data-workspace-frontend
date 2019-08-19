ac595692fa5d184f2b9ef58ebe37f1bd63510df0
Nick Ross <nick.ir.ross@gmail.com>
2019-08-19 15:26:10 +0100

    Remove unncessary use of model attribute

--- dataworkspace/dataworkspace/apps/datasets/migrations/0004_auto_20190813_1536.py
+++ dataworkspace/dataworkspace/apps/datasets/migrations/0004_auto_20190813_1536.py
@@ -6,15 +6,15 @@ from django.db import migrations, connection, ProgrammingError
 def generate_table_name(apps, _):
     model = apps.get_model('datasets', 'ReferenceDataset')
     for r in model.objects.all():
-        r._original_table_name = 'refdata__{}'.format(r.id)
-        if r.table_name is None or r.table_name == r._original_table_name:
+        original_table_name = 'refdata__{}'.format(r.id)
+        if r.table_name is None or r.table_name == original_table_name:
             r.table_name = 'ref_{}'.format(r.slug.replace('-', '_'))
-            print('Changing table "{}" to "{}"'.format(r._original_table_name, r.table_name))
+            print('Changing table "{}" to "{}"'.format(original_table_name, r.table_name))
             r.schema_version += 1
             r.save()
             with connection.schema_editor() as editor:
                 try:
-                    editor.alter_db_table(model, r._original_table_name, r.table_name)
+                    editor.alter_db_table(model, original_table_name, r.table_name)
                 except ProgrammingError:
                     pass
 
