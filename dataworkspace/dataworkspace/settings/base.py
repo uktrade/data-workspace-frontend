9e756d1e1ce2a3e5f4e0076aa9f0f173906bf867
Alexey Bezhan <al@fueledbylemons.com>
2019-10-24 16:30:41 +0100

    Add a dataset eligibility criteria DB field
    Makes it possible to store a list of access requirements for each
    dataset.
    
    The criteria are (potentially) different between each dataset, so
    for now we're storing them as a list of strings (as opposed to a
    many-to-many relationship with a new table).
    
    This is using a postgres ArrayField to store data since it's the
    easiest type to setup with Django admin and fits the data quite
    well. One possible alternative would be a single text field, but
    it would require us to use a special character (a newline for example)
    as a delimiter between different criteria in order to render them
    as bullet points.

--- dataworkspace/dataworkspace/settings/base.py
+++ dataworkspace/dataworkspace/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.postgres',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'adminsortable2',
