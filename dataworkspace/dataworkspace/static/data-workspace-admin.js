25f4f15a56d418557b53f7b1f65db2b84bcfb043
David Voong <david.voong@digital.trade.gov.uk>
2019-10-31 15:31:58 +0000

    add column name generator functions

--- dataworkspace/dataworkspace/static/data-workspace-admin.js
+++ dataworkspace/dataworkspace/static/data-workspace-admin.js
@@ -31,4 +31,29 @@ $(function(){
     tableNameInput.on('keyup', function(){
 	_changed = true;
     });
+
+    $('.field-name input').on('keyup', function(){
+	let name = $(this).attr('name');
+	let index = name.match(/(?!=fields-)[0-9]+(?=-name)/)
+	let columnField = $(`.field-column_name input[name="fields-${index}-column_name"]`);
+	if(!columnField[0].hasAttribute('_change')){
+	    columnField.attr('_change', 0)
+	}
+
+	if(parseInt(columnField.attr('_change')) == 0){
+	    let columnName;
+	    let urlified = URLify($(this).val());
+	    if(urlified == ''){
+		columnName = ''
+	    } else {
+		columnName = urlified.split('-').join('_');
+	    }
+	    columnField.val(columnName)
+	}
+    });
+
+    $('.field-column_name input').on('keyup', function(){
+	$(this).attr('_change', 1);
+    });
 });
+
