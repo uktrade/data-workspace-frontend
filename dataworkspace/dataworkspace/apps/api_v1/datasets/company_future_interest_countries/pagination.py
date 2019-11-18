08ac5b6692e2d57a153c53dc08e0f337383d2c5f
David Voong <david.voong@digital.trade.gov.uk>
2019-11-18 15:08:32 +0000

    remove pagination class, make it inline in the view

--- dataworkspace/dataworkspace/apps/api_v1/datasets/company_future_interest_countries/pagination.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.pagination import CursorPagination
-
-
-class CompanyFutureInterestCountriesDatasetViewCursorPagination(CursorPagination):
-    """
-    Cursor Pagination for CompanyFutureInterestCountries
-    """
-
-    ordering = ('id', )
