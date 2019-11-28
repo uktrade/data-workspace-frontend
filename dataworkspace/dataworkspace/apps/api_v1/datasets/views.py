253d84f9389896e64dba8a235554bb580a1c148b
David Voong <david.voong@digital.trade.gov.uk>
2019-11-28 14:11:38 +0000

    spike using michals code for streaming http response

--- dataworkspace/dataworkspace/apps/api_v1/datasets/views.py
+++ dataworkspace/dataworkspace/apps/api_v1/datasets/views.py
@@ -1,9 +1,239 @@
+import json, psycopg2, re
 from django.http import HttpResponseNotFound
-
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, StreamingHttpResponse
 from rest_framework.pagination import CursorPagination
 from rest_framework.views import APIView
 from dataworkspace.apps.datasets.models import SourceTable
+from dataworkspace.apps.core.utils import database_dsn
+from django.conf import settings
+
+
+def get_postgres_column_names_data_types(sourcetable):
+    with psycopg2.connect(
+        database_dsn(settings.DATABASES_DATA[sourcetable.database.memorable_name])
+    ) as conn, conn.cursor() as cur:
+        cur.execute(
+            '''
+            SELECT
+                pg_attribute.attname AS column_name,
+                pg_catalog.format_type(pg_attribute.atttypid, pg_attribute.atttypmod) AS data_type
+            FROM
+                pg_catalog.pg_attribute
+            INNER JOIN
+                pg_catalog.pg_class ON pg_class.oid = pg_attribute.attrelid
+            INNER JOIN
+                pg_catalog.pg_namespace ON pg_namespace.oid = pg_class.relnamespace
+            WHERE
+                pg_attribute.attnum > 0
+                AND NOT pg_attribute.attisdropped
+                AND pg_namespace.nspname = %s
+                AND pg_class.relname = %s
+            ORDER BY
+                attnum ASC;
+        ''',
+            (sourcetable.schema, sourcetable.table),
+        )
+        return cur.fetchall()
+
+
+def get_primary_keys(connection, table, schema='public'):
+    sql = '''
+    SELECT
+        pg_attribute.attname AS column_name
+    FROM
+        pg_catalog.pg_class pg_class_table
+    INNER JOIN
+        pg_catalog.pg_index ON pg_index.indrelid = pg_class_table.oid
+    INNER JOIN
+        pg_catalog.pg_class pg_class_index ON pg_class_index.oid = pg_index.indexrelid
+    INNER JOIN
+        pg_catalog.pg_namespace ON pg_namespace.oid = pg_class_table.relnamespace
+    INNER JOIN
+        pg_catalog.pg_attribute ON pg_attribute.attrelid = pg_class_index.oid
+    WHERE
+        pg_namespace.nspname = %s
+        AND pg_class_table.relname = %s
+        AND pg_index.indisprimary
+    ORDER BY
+        pg_attribute.attnum
+    '''
+    with connection.cursor() as cursor:
+        cursor.execute(sql, [schema, table])
+        rows = cursor.fetchall()
+    return [row[0] for row in rows]
+
+def get_rows(sourcetable, schema_value_funcs, pagination):
+    cursor_itersize = 1000
+
+    # Order the rows by primary key so
+    # - multiple requests are consistent with each other;
+    # - and specifically and more importantly, sopaginated results are
+    #   consistent with each other
+    # We make no assumption on the name or number of columns in the primary
+    # key, other than it exists
+    # We _could_ use `oid` to order rows if there is no primary key, but we
+    # would like all tables to have a primary key, so we deliberately don't
+    # implement this
+    with psycopg2.connect(
+        database_dsn(settings.DATABASES_DATA[sourcetable.database.memorable_name])
+    ) as conn, conn.cursor() as cur:
+
+        cur.execute(
+            '''
+            SELECT
+                pg_attribute.attname AS column_name
+            FROM
+                pg_catalog.pg_class pg_class_table
+            INNER JOIN
+                pg_catalog.pg_index ON pg_index.indrelid = pg_class_table.oid
+            INNER JOIN
+                pg_catalog.pg_class pg_class_index ON pg_class_index.oid = pg_index.indexrelid
+            INNER JOIN
+                pg_catalog.pg_namespace ON pg_namespace.oid = pg_class_table.relnamespace
+            INNER JOIN
+                pg_catalog.pg_attribute ON pg_attribute.attrelid = pg_class_index.oid
+            WHERE
+                pg_namespace.nspname = %s
+                AND pg_class_table.relname = %s
+                AND pg_index.indisprimary
+            ORDER BY
+                pg_attribute.attnum
+        ''',
+            (sourcetable.schema, sourcetable.table),
+        )
+        primary_key_column_names = [row[0] for row in cur.fetchall()]
+
+    with psycopg2.connect(
+        database_dsn(settings.DATABASES_DATA[sourcetable.database.memorable_name])
+    ) as conn, conn.cursor(
+        name='google_data_studio_all_table_data'
+    ) as cur:  # Named cursor => server-side cursor
+
+        cur.itersize = cursor_itersize
+        cur.arraysize = cursor_itersize
+
+        fields_sql = psycopg2.sql.SQL(',').join(
+            [psycopg2.sql.Identifier(schema['name']) for schema, _ in schema_value_funcs]
+        )
+        primary_key_sql = psycopg2.sql.SQL(',').join(
+            [psycopg2.sql.Identifier(column_name) for column_name in primary_key_column_names]
+        )
+        schema_sql = psycopg2.sql.Identifier(sourcetable.schema)
+        table_sql = psycopg2.sql.Identifier(sourcetable.table)
+
+        def query_var_non_paginated():
+            return (
+                psycopg2.sql.SQL(
+                    '''
+                SELECT {} FROM {}.{} ORDER BY {}
+            '''
+                ).format(fields_sql, schema_sql, table_sql, primary_key_sql),
+                (),
+            )
+
+        def query_vars_paginated():
+            limit = int(pagination['rowCount'])
+            offset = (
+                int(pagination['startRow']) - 1
+            )  # Google Data Studio start is 1-indexed
+            return (
+                psycopg2.sql.SQL(
+                    '''
+                SELECT {} FROM {}.{} ORDER BY {} LIMIT %s OFFSET %s
+            '''
+                ).format(fields_sql, schema_sql, table_sql, primary_key_sql),
+                (limit, offset),
+            )
+
+        query_sql, vars_sql = (
+            query_var_non_paginated() if pagination is None else query_vars_paginated()
+        )
+
+        cur.execute(query_sql, vars_sql)
+
+        while True:
+            rows = cur.fetchmany(cursor_itersize)
+            print('\033[34mrows\033[0m:', rows)
+            # for row in rows:
+            #     yield json.dumps(
+            #         {
+            #             'values': [
+            #                 schema_value_funcs[i][1](value)
+            #                 for i, value in enumerate(row)
+            #             ]
+            #         }
+            #     ).encode('utf-8')
+            if not rows:
+                break
+            yield json.dumps(rows).encode('utf-8')
+
+def get_schema(schema_value_funcs):
+    return [schema for schema, _ in schema_value_funcs]
+
+def schema_value_func_for_data_type(data_type):
+    return next(
+        (schema, value_func)
+        for data_type_pattern, schema, value_func in SCHEMA_DATA_TYPE_PATTERNS
+        if re.match(data_type_pattern, data_type)
+    )
+
+
+def schema_value_func_for_data_types(sourcetable):
+    return [
+        (
+            {
+                'name': column_name,
+                'label': column_name.replace('_', ' ').capitalize(),
+                **schema,
+            },
+            value_func,
+        )
+        for column_name, data_type in get_postgres_column_names_data_types(sourcetable)
+        for schema, value_func in [schema_value_func_for_data_type(data_type)]
+    ]
+
+
+SCHEMA_STRING = {'dataType': 'STRING', 'semantics': {'conceptType': 'DIMENSION'}}
+
+SCHEMA_STRING_DATE = {
+    'dataType': 'STRING',
+    'semantics': {'conceptType': 'DIMENSION', 'semanticType': 'YEAR_MONTH_DAY'},
+}
+
+SCHEMA_STRING_DATE_TIME = {
+    'dataType': 'STRING',
+    'semantics': {'conceptType': 'DIMENSION', 'semanticType': 'YEAR_MONTH_DAY_SECOND'},
+}
+
+SCHEMA_BOOLEAN = {'dataType': 'BOOLEAN', 'semantics': {'conceptType': 'DIMENSION'}}
+
+SCHEMA_NUMBER = {'dataType': 'NUMBER', 'semantics': {'conceptType': 'METRIC'}}
+
+SCHEMA_DATA_TYPE_PATTERNS = (
+    (r'^(character varying.*)|(text)$', SCHEMA_STRING, lambda v: v),
+    (r'^(uuid)$', SCHEMA_STRING, str),
+    (
+        # Not sure if this is suitable for Google Data Studio analysis, but avoids the error if
+        # passing an array as a value:
+        # "The data returned from the community connector is malformed"
+        r'^text\[\]$',
+        SCHEMA_STRING,
+        ','.join,
+    ),
+    (
+        r'^date$',
+        SCHEMA_STRING_DATE,
+        lambda v: v.strftime('%Y%m%d') if v is not None else None,
+    ),
+    (
+        r'^timestamp.*$',
+        SCHEMA_STRING_DATE_TIME,
+        lambda v: v.strftime('%Y%m%d%H%M%S') if v is not None else None,
+    ),
+    (r'^boolean$', SCHEMA_BOOLEAN, lambda v: v),
+    (r'^(bigint)|(decimal)|(integer)|(numeric)|(real)$', SCHEMA_NUMBER, lambda v: v),
+)
 
 
 class Pagination(CursorPagination):
@@ -16,20 +246,40 @@ class APIDatasetView(APIView):
     A GET API view to return the data for the company future countries of interest dataset
     """
 
+    cursor_itersize = 1000 #todo make this configurable
+    cursor_arraysize = 1000 #todo make this configurable
+    
     def get(self, request, dataset_id, source_table_id):
-        # dataset_id = '14983f3e-c557-4175-8dad-7f1a6942d949'
-        # source_table_id = '5a2ee5dd-f025-4939-b0a1-bb85ab7504d7'
-        whitelist = ['14983f3e-c557-4175-8dad-7f1a6942d949']
-        if dataset_id in whitelist:
-            # source_table = get_object_or_404(
-            #     SourceTable,
-            #     id=source_table_id,
-            #     dataset__id=dataset_id
-            # )
-            paginator = Pagination()
-            page = paginator.paginate_queryset(
-                SourceTable.objects.values(), request, view=self
-            )
-            return paginator.get_paginated_response(page)
-        else:
-            return HttpResponseNotFound('Invalid dataset id')
+        pagination = None
+        source_table = get_object_or_404(
+            SourceTable,
+            id=source_table_id,
+            dataset__id=dataset_id
+        )
+
+        column_names = []
+        
+        schema_value_funcs = [
+            (schema, value_func)
+            for schema, value_func in schema_value_func_for_data_types(source_table)
+            if schema['name'] in column_names or column_names == []
+        ]
+        def yield_schema_and_row_bytes():
+            yield b'{"headers":' + json.dumps(
+                [x['name'] for x in get_schema(schema_value_funcs)]
+            ).encode(
+                'utf-8'
+            ) + b',"values":'
+
+            later_row = False
+            for row in get_rows(source_table, schema_value_funcs, pagination):
+                if later_row:
+                    yield b',' + row
+                else:
+                    yield row
+                later_row = True
+            yield b'}'
+
+        return StreamingHttpResponse(
+            yield_schema_and_row_bytes(), content_type='application/json', status=200,
+        )
--- dataworkspace/dataworkspace/tests/api_v1/datasets/test_views.py
+++ dataworkspace/dataworkspace/tests/api_v1/datasets/test_views.py
@@ -1,20 +1,95 @@
+import json
+from unittest.mock import Mock, patch
+from psycopg2 import connect, sql
 from django.test import TestCase
 from django.urls import resolve
+from django.conf import settings
+from dataworkspace.apps.core.models import Database
+from dataworkspace.apps.core.utils import database_dsn
+from dataworkspace.apps.datasets.models import DataGrouping, DataSet, SourceTable
 
+
+def flush_database(connection):
+    connection.autocommit = True
+    with connection.cursor() as cursor:
+        sql = 'select current_user'
+        cursor.execute(sql)
+        current_user = cursor.fetchone()[0]
+        sql = 'DROP SCHEMA public CASCADE;'
+        cursor.execute(sql)
+        sql = 'CREATE SCHEMA public;'
+        cursor.execute(sql)
+        sql = 'GRANT ALL ON SCHEMA public TO {};'.format(current_user)
+        cursor.execute(sql)
+        sql = 'GRANT ALL ON SCHEMA public TO {};'.format(current_user)
+        cursor.execute(sql)
 
 class TestAPIDatasetView(TestCase):
 
-    url = '/api/v1/dataset/future-interest-countries/table-id'
+    def flush_database(self):
+        with connect(
+                database_dsn(settings.DATABASES_DATA[self.memorable_name])
+        ) as conn:
+            flush_database(conn)
 
+    def setUp(self):
+        self.table = 'test_source_table'
+        self.memorable_name = 'test_external_db'
+        self.flush_database()
+
+    def tearDown(self):
+        self.flush_database()
+    
     def test_route(self):
-        resolver = resolve(self.url)
+        url = '/api/v1/dataset/future-interest-countries/table-id'
+        resolver = resolve(url)
         self.assertEqual(resolver.view_name, 'api-v1:dataset:api-dataset-view')
 
     def test_data(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
 
-    def test_unauthorised_dataset(self):
-        url = '/api/v1/dataset/restricted-dataset/table-id'
+        # create django objects
+        memorable_name = self.memorable_name
+        table = self.table
+        database = Database.objects.create(memorable_name=memorable_name)
+        data_grouping = DataGrouping.objects.create()
+        dataset = DataSet.objects.create(grouping=data_grouping, volume=0)
+        source_table = SourceTable.objects.create(
+            dataset=dataset,
+            database=database,
+            table=table,
+        )
+
+        # create external source table
+        with connect(
+                database_dsn(settings.DATABASES_DATA[memorable_name])
+        ) as conn, conn.cursor() as cur:
+            sql = '''
+            create table {table} (id int primary key, name varchar(100))
+            '''.format(table=table)
+            cur.execute(sql)
+            sql = '''insert into {table} values (%s, %s)'''.format(table=self.table)
+            values = [(0, 'abigail'), (1, 'romeo')]
+            cur.executemany(sql, values)
+            
+        url = '/api/v1/dataset/{}/{}'.format(dataset.id, source_table.id)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
+        expected = {
+            'headers': ['id', 'name'],
+            'values': [
+                [0, 'abigail'],
+                [1, 'romeo'],
+            ]
+        }
+
+        print('\033[34mresponse\033[0m:', response)
+        print('\033[34mresponse.streaming_content\033[0m:', response.streaming_content)
+        output = b''
+        for streaming_output in response.streaming_content:
+            output = output + streaming_output
+        print('\033[34mboutput\033[0m:', output)
+        print('\033[34moutput\033[0m:', output.decode('utf-8'))
+        output_dict = json.loads(output.decode('utf-8'))
+        print('output_dict:', output_dict)
+        self.assertEqual(output_dict, expected)
+        
