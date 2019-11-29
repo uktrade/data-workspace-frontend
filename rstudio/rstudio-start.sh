875e1eab0d9d02ae83d98694b54fc8138f683d20
Michal Charemza <michal@charemza.name>
2019-11-29 09:16:05 +0000

    (feat) Add library to access S3 from R

--- rstudio/rstudio-start.sh
+++ rstudio/rstudio-start.sh
@@ -4,4 +4,8 @@ set -e
 
 mkdir -p /etc/rstudio/connections
 python3 rstudio-db-creds.py
+
+echo "S3_PREFIX='${S3_PREFIX}'" >> /home/rstudio/.Renviron
+echo "AWS_DEFAULT_REGION='${AWS_DEFAULT_REGION}'" >> /home/rstudio/.Renviron
+echo "AWS_CONTAINER_CREDENTIALS_RELATIVE_URI='${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}'" >> /home/rstudio/.Renviron
 /usr/lib/rstudio-server/bin/rserver
