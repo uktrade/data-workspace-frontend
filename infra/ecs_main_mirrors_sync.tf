2d0fd297c82202537c7befcb2a18b1bf321525d6
Michal Charemza <michal@charemza.name>
2019-12-05 13:48:33 +0000

    (feat) Infrastructure for mirroring Debian

--- infra/ecs_main_mirrors_sync.tf
+++ infra/ecs_main_mirrors_sync.tf
@@ -32,6 +32,7 @@ data "template_file" "mirrors_sync_container_definitions_conda" {
     mirror_anaconda_conda_anaconda = "True"
     mirror_cran = "False"
     mirror_pypi = "False"
+    mirror_debian = "False"
   }
 }
 
@@ -69,6 +70,7 @@ data "template_file" "mirrors_sync_container_definitions_cran" {
     mirror_anaconda_conda_anaconda = "False"
     mirror_cran = "True"
     mirror_pypi = "False"
+    mirror_debian = "False"
   }
 }
 
@@ -106,6 +108,45 @@ data "template_file" "mirrors_sync_container_definitions_pypi" {
     mirror_anaconda_conda_anaconda = "False"
     mirror_cran = "False"
     mirror_pypi = "True"
+    mirror_debian = "False"
+  }
+}
+
+resource "aws_ecs_task_definition" "mirrors_sync_debian" {
+  count = "${var.mirrors_bucket_name != "" ? 1 : 0}"
+  family                = "jupyterhub-mirrors-sync-debian"
+  container_definitions = "${data.template_file.mirrors_sync_container_definitions_debian.rendered}"
+  execution_role_arn    = "${aws_iam_role.mirrors_sync_task_execution.arn}"
+  task_role_arn         = "${aws_iam_role.mirrors_sync_task.arn}"
+  network_mode          = "awsvpc"
+  cpu                   = "${local.mirrors_sync_container_cpu}"
+  memory                = "${local.mirrors_sync_container_memory}"
+  requires_compatibilities = ["FARGATE"]
+}
+
+data "template_file" "mirrors_sync_container_definitions_debian" {
+  count = "${var.mirrors_bucket_name != "" ? 1 : 0}"
+  template = "${file("${path.module}/ecs_main_mirrors_sync_container_definitions.json")}"
+
+  vars {
+    container_image    = "quay.io/uktrade/data-workspace-mirrors-sync:debian-mirrors"
+    container_name     = "${local.mirrors_sync_container_name}"
+    container_cpu      = "${local.mirrors_sync_container_cpu}"
+    container_memory   = "${local.mirrors_sync_container_memory}"
+
+    log_group  = "${aws_cloudwatch_log_group.mirrors_sync.name}"
+    log_region = "${data.aws_region.aws_region.name}"
+
+    mirrors_bucket_region = "${data.aws_region.aws_region.name}"
+    mirrors_bucket_host = "s3-${data.aws_region.aws_region.name}.amazonaws.com"
+    mirrors_bucket_name = "${var.mirrors_bucket_name}"
+
+    mirror_anaconda_r = "False"
+    mirror_anaconda_conda_forge = "False"
+    mirror_anaconda_conda_anaconda = "False"
+    mirror_cran = "False"
+    mirror_pypi = "False"
+    mirror_debian = "True"
   }
 }
 
