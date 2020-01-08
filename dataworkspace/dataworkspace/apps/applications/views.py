2d02cae0104b275224266a3fe380f07ac1288d3e
Nick Ross <nick.ir.ross@gmail.com>
2020-01-08 10:12:01 +0000

    Display a random data protection message on the tool spawning page

--- dataworkspace/dataworkspace/apps/applications/views.py
+++ dataworkspace/dataworkspace/apps/applications/views.py
@@ -1,6 +1,5 @@
-import datetime
 import hashlib
-import math
+import random
 
 from django.conf import settings
 from django.contrib import messages
@@ -19,6 +18,37 @@ from dataworkspace.apps.applications.utils import stop_spawner_and_application
 from dataworkspace.apps.core.views import public_error_500_html_view
 
 
+TOOL_LOADING_MESSAGES = [
+    {
+        'title': 'Principle 1 of the Data Protection Act 2018',
+        'body': 'You must make sure that information is used fairly, lawfully and transparently.',
+    },
+    {
+        'title': 'Principle 2 of the Data Protection Act 2018',
+        'body': 'You must make sure that information is used for specified, explicit purposes.',
+    },
+    {
+        'title': 'Principle 3 of the Data Protection Act 2018',
+        'body': 'You must make sure that information is used in a way that is adequate, relevant '
+        'and limited to only what is necessary.',
+    },
+    {
+        'title': 'Principle 4 of the Data Protection Act 2018',
+        'body': 'You must make sure that information is accurate and, where necessary, kept up to date.',
+    },
+    {
+        'title': 'Principle 5 of the Data Protection Act 2018',
+        'body': 'You must make sure that information is kept for no longer than is necessary.',
+    },
+    {
+        'title': 'Principle 6 of the Data Protection Act 2018',
+        'body': 'You must make sure that information is handled in a way that ensures appropriate '
+        'security, including protection against unlawful or unauthorised processing, access, '
+        'loss, destruction or damage.',
+    },
+]
+
+
 def application_spawning_html_view(request, public_host):
     return (
         application_spawning_html_GET(request, public_host)
@@ -34,37 +64,14 @@ def application_spawning_html_GET(request, public_host):
         )
     except ApplicationInstance.DoesNotExist:
         return public_error_500_html_view(request)
-    else:
-        # There is some duplication between this and the front end, but
-        # we avoid the occasional flash if missing content before the
-        # front end renders the time remaining
-        expected_total = application_instance.application_template.spawner_time
-        now = datetime.datetime.now().timestamp()
-        created = application_instance.created_date.timestamp()
-        seconds_remaining_float = max(0, created + expected_total - now)
-        seconds_remaining = math.ceil(seconds_remaining_float)
-        seconds = seconds_remaining % 60
-        minutes = int((seconds_remaining - seconds) / 60)
-        memory = application_instance.memory
-        cpu = application_instance.cpu
-        cpu_memory_components = (
-            [str(int(cpu) / 1024).rstrip('0').rstrip('.') + ' CPU']
-            if cpu is not None
-            else []
-        ) + (
-            [str(int(memory) / 1024).rstrip('0').rstrip('.') + ' GB of memory']
-            if memory is not None
-            else []
-        )
-        cpu_memory = ' and '.join(cpu_memory_components)
-        cpu_memory_string = ('with ' + cpu_memory) if cpu_memory else ''
-        context = {
-            'seconds_remaining_float': seconds_remaining_float,
-            'time_remaining': f'{minutes}:{seconds:02}',
-            'application_nice_name': application_instance.application_template.nice_name,
-            'cpu_memory_string': cpu_memory_string,
-        }
-        return render(request, 'spawning.html', context, status=202)
+
+    context = {
+        'application_nice_name': application_instance.application_template.nice_name,
+        'loading_message': TOOL_LOADING_MESSAGES[
+            random.randint(0, len(TOOL_LOADING_MESSAGES) - 1)
+        ],
+    }
+    return render(request, 'spawning.html', context, status=202)
 
 
 def tools_html_view(request):
