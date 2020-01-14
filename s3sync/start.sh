424f81e8e09e11ace4f61e57865a69b63154af06
Michal Charemza <michal@charemza.name>
2020-01-14 11:56:39 +0000

    (feat) Add git to tools

--- s3sync/start.sh
+++ s3sync/start.sh
@@ -5,10 +5,12 @@ set -e
 # Path-style even though it's deprecated. The bucket names have dots in, and
 # at the time of writing the host-style certs returned by AWS are wildcards
 # and don't support dots in the bucket name
-# Excluding .checkpoints to not have issues with jupyters3 .checkpoints
-# folders that are subfolders of files in S3
-# Excluding all all dot files locally since they are a) probably unnecessary
-# and b) contains a sqlite database for temporary data
+# Excluding .checkpoints from remote to not have issues with jupyters3
+# .checkpoints folders that are subfolders of files in S3. Other dot files
+# already on the remote _are_ synced to local
+# Excluding all all dot files locally, except .git, since they
+# a) are probably unnecessary
+# b) contain a sqlite database for temporary data
 mobius3 \
     /home/s3sync/data \
     https://s3-${S3_REGION}.amazonaws.com/${S3_BUCKET}/ \
@@ -17,4 +19,4 @@ mobius3 \
     --log-level INFO \
     --credentials-source ecs-container-endpoint \
     --exclude-remote '(.*(/|^)\.checkpoints/)|(.*(/|^)bigdata/.*)' \
-    --exclude-local '(.*/\..*)|(.*(/|^)bigdata/.*)'
+    --exclude-local '^(?!.*/\.git($|(/.+)))((.*/\..*)|(.*(/|^)bigdata/.*))'
