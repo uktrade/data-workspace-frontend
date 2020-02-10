57b58e74f0f4990e3ea7ae8b727182940dd68297
Nick Ross <nick.ir.ross@gmail.com>
2020-02-10 13:22:52 +0000

    Double queue timeout to 2 minutes for streaming downloads

--- dataworkspace/dataworkspace/apps/core/utils.py
+++ dataworkspace/dataworkspace/apps/core/utils.py
@@ -390,7 +390,7 @@ def streaming_query_response(user_email, database, query, filename):
     logger.info('streaming_query_response start: %s %s %s', user_email, database, query)
     cursor_itersize = 1000
     queue_size = 3
-    queue_timeout = 60
+    queue_timeout = 60 * 2
     bytes_queue = gevent.queue.Queue(maxsize=queue_size)
 
     def put_db_rows_to_queue():
@@ -470,7 +470,7 @@ def streaming_query_response(user_email, database, query, filename):
     put_db_rows_to_queue_job = gevent.spawn(put_db_rows_to_queue)
     put_db_rows_to_queue_job.link_exception(handle_exception)
 
-    response = StreamingHttpResponse(yield_bytes_from_queue(), content_type='text/csv')
+    response = StreamingHttpResponse(yield_bytes_from_queue(),  content_type='text/csv')
     response['Content-Disposition'] = f'attachment; filename="{filename}"'
     return response
 
