8802553fe4e3aa4b9493668d2394ff821d097e58
Samuel Williams <samuelhwilliams@gmail.com>
2020-03-18 12:14:22 +0000

    Update support and feedback links/content
    Add a link to 'support and feedback' to the heaader/footer, and update
    the content on the page to help users understand what the form is for.
    
    Ticket: https://trello.com/c/HYMb3J4Z/908

--- dataworkspace/dataworkspace/apps/core/forms.py
+++ dataworkspace/dataworkspace/apps/core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 
 class SupportForm(forms.Form):
@@ -11,9 +12,21 @@ class SupportForm(forms.Form):
         required=True,
         label='Description',
         widget=forms.Textarea(attrs={'class': 'govuk-textarea'}),
+        #
+        # If you're here because you want to copy the help text (i.e. bullets as form hints), then please don't. If
+        # this needs to be reused, we should probably do something else (e.g. convert it to markdown and add a markdown
+        # filter that can output GOV.UK Design System-aware elements). So this HTML-in-code should either remain an
+        # exception or eventually disappear.
         help_text=(
-            'If you want to provide feedback or a suggestion, describe it here. '
-            'If you were having a problem, explain what you did, what happened and '
-            'what you expected to happen.'
+            mark_safe(
+                """
+<p class="govuk-hint">Please use this form to give us feedback, report a technical issue or request data that is not available on Data Workspace.</p>
+<p class="govuk-hint">If you had a technical issue, briefly explain:</p>
+<ul class="govuk-list govuk-list--bullet govuk-hint">
+  <li>what you did</li>
+  <li>what happened</li>
+  <li>what you expected to happen</li>
+</ul>"""
+            )
         ),
     )
