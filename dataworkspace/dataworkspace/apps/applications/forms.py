35c6ea3658ba945d069928fd672612150087b67a
Samuel Williams <samuelhwilliams@gmail.com>
2020-04-20 11:18:35 +0100

    Use plain text fields for contact emails
    We have a lot of users, so using select fields is not really feasible or
    desirable any more (slow and leaking user data). So we'll switch these
    fields to basic text fields with some extra validation for
    user-friendliness.
    
    Ticket: https://trello.com/c/CvOWjmvD/978

--- dataworkspace/dataworkspace/apps/applications/forms.py
+++ dataworkspace/dataworkspace/apps/applications/forms.py
@@ -1,10 +1,60 @@
-from django.forms import Textarea
+from django.contrib.auth import get_user_model
+from django.core.validators import EmailValidator
+from django.forms import Textarea, TextInput, ModelChoiceField
 
 from dataworkspace.apps.datasets.models import VisualisationCatalogueItem
 from dataworkspace.forms import GOVUKDesignSystemModelForm
 
 
+class _NiceEmailValidationModelChoiceField(ModelChoiceField):
+    def clean(self, value):
+        if value:
+            EmailValidator(message=self.error_messages['invalid_email'])(value)
+        return super().clean(value)
+
+
 class VisualisationsUICatalogueItemForm(GOVUKDesignSystemModelForm):
+    enquiries_contact = _NiceEmailValidationModelChoiceField(
+        queryset=get_user_model().objects.all(),
+        to_field_name='email',
+        widget=TextInput,
+        required=False,
+        error_messages={
+            "invalid_email": "Enter a valid email address for the enquiries contact",
+            "invalid_choice": "The enquiries contact must have previously visited Data Workspace",
+        },
+    )
+    secondary_enquiries_contact = _NiceEmailValidationModelChoiceField(
+        queryset=get_user_model().objects.all(),
+        to_field_name='email',
+        widget=TextInput,
+        required=False,
+        error_messages={
+            "invalid_email": "Enter a valid email address for the secondary enquiries contact",
+            "invalid_choice": "The secondary enquiries contact must have previously visited Data Workspace",
+        },
+    )
+    information_asset_manager = _NiceEmailValidationModelChoiceField(
+        queryset=get_user_model().objects.all(),
+        to_field_name='email',
+        widget=TextInput,
+        required=False,
+        error_messages={
+            "invalid_email": "Enter a valid email address for the information asset manager",
+            "invalid_choice": "The information asset manager must have previously visited Data Workspace",
+        },
+    )
+    information_asset_owner = _NiceEmailValidationModelChoiceField(
+        queryset=get_user_model().objects.all(),
+        to_field_name='email',
+        widget=TextInput,
+        required=False,
+        error_messages={
+            "invalid_email": "Enter a valid email address for the information asset owner",
+            "invalid_choice": "The information asset owner must have previously visited Data Workspace",
+        },
+    )
+
     class Meta:
         model = VisualisationCatalogueItem
         fields = [
@@ -27,3 +77,15 @@ class VisualisationsUICatalogueItemForm(GOVUKDesignSystemModelForm):
         self.fields['short_description'].error_messages[
             'required'
         ] = "The visualisation must have a summary"
+
+        self._email_fields = [
+            'enquiries_contact',
+            'secondary_enquiries_contact',
+            'information_asset_manager',
+            'information_asset_owner',
+        ]
+
+        # Set the form field data for email fields to the actual user email address - by default it's the User ID.
+        for field in self._email_fields:
+            if getattr(self.instance, field):
+                self.initial[field] = getattr(self.instance, field).email
