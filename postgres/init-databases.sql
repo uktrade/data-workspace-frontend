b4cf4600378b9f40e1266d5e8ea86edd8d82f32a
Michal Charemza <michal@charemza.name>
2020-04-25 15:30:59 +0100

    (tests,refactor) Use generate_series to more easily generate more test data

--- postgres/init-databases.sql
+++ postgres/init-databases.sql
@@ -6,15 +6,18 @@ CREATE DATABASE testdb2;
 
 \c datasets;
 CREATE TABLE test_dataset (id int primary key, data text);
-INSERT INTO test_dataset VALUES (1, 'test data 1');
-INSERT INTO test_dataset VALUES (2, 'test data 2');
+INSERT INTO test_dataset(id, data)
+SELECT i, concat('test data ', i::text)
+FROM generate_series(1, 2) AS t(i);
 
 \c testdb1;
 CREATE TABLE dataset_1 (id int primary key, data text);
-INSERT INTO dataset_1 VALUES (1, 'test data 1');
-INSERT INTO dataset_1 VALUES (2, 'test data 2');
+INSERT INTO dataset_1(id, data)
+SELECT i, concat('test data ', i::text)
+FROM generate_series(1, 2) AS t(i);
 
 \c testdb2;
 CREATE TABLE dataset_2 (id int primary key, data text);
-INSERT INTO dataset_2 VALUES (1, 'test data 1');
-INSERT INTO dataset_2 VALUES (2, 'test data 2');
+INSERT INTO dataset_2(id, data)
+SELECT i, concat('test data ', i::text)
+FROM generate_series(1, 2) AS t(i);
