99c5d5baa49cf7e24a767c9db237ca6e92ec71d9
Nick Ross <nick.ir.ross@gmail.com>
2020-05-06 12:05:29 +0100

    Fix bug where proxy was checking for incorrect accounts url

--- dataworkspace/proxy.py
+++ dataworkspace/proxy.py
@@ -186,7 +186,7 @@ async def async_main():
             request.url.path.startswith('/api/v1/dataset/')
             or request.url.path.startswith('/api/v1/reference-dataset/')
             or request.url.path.startswith('/api/v1/eventlog/')
-            or request.url.path.startswith('/api/v1/accounts/')
+            or request.url.path.startswith('/api/v1/account/')
             or request.url.path.startswith('/api/v1/application-instance/')
             and request.url.host == root_domain_no_port
         )
