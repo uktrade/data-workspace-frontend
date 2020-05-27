01eb78e7d2a809dc5f13da42f1c1ac472fe2c4e1
Michal Charemza <michal@charemza.name>
2020-05-27 11:56:23 +0100

    (feat) s3sync is Fargate 1.4.0 compatible
    s3sync is a sidecar container running alongside tools: they share a
    volume to both write to and read from.
    
    Fargate 1.3.0, seems to preserve the ownership from the containers, the
    s3sync user and group. However, in 1.4.0, it seems to now be owned by
    root. To write to the volume, there seems to be little alternative than
    to start the container as root.
    
    However, we do this to change ownership of the files in the volume and
    then run the s3sync proper as the lower-privilage s3sync user.

--- s3sync/start.sh
+++ s3sync/start.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+chown -R s3sync:s3sync /home/s3sync/data
+
 # Path-style even though it's deprecated. The bucket names have dots in, and
 # at the time of writing the host-style certs returned by AWS are wildcards
 # and don't support dots in the bucket name
@@ -10,7 +12,7 @@ set -e
 # already on the remote _are_ synced to local
 # Exclude .__mobius3_flush__ files locally, since there is a suspected bug
 # in mobius3 where they can end up being uploaded
-mobius3 \
+sudo -E -u s3sync mobius3 \
     /home/s3sync/data \
     ${S3_BUCKET} \
     https://s3-${S3_REGION}.amazonaws.com/{}/ \
