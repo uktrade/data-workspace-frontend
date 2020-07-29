6adef321e0b0583ca6afb6c021947f1ab8fea5f5
Michal Charemza <michal@charemza.name>
2020-07-29 17:59:43 +0100

    Add sentry_environment to each tool task definition

--- infra/ecs_notebooks_jupyterlab_python.tf
+++ infra/ecs_notebooks_jupyterlab_python.tf
@@ -57,6 +57,7 @@ data "template_file" "jupyterlabpython_container_definitions" {
     log_region = "${data.aws_region.aws_region.name}"
 
     sentry_dsn = "${var.sentry_dsn}"
+    sentry_environment = "${var.admin_environment}"
 
     metrics_container_image = "${var.metrics_container_image}:${data.external.jupyterlabpython_metrics_current_tag.result.tag}"
     s3sync_container_image = "${var.s3sync_container_image}:${data.external.jupyterlabpython_s3sync_current_tag.result.tag}"
