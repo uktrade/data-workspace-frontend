a34b207f5827ee310373ec618d98f2b621825f75
Samuel Williams <samuelhwilliams@gmail.com>
2020-10-09 16:10:44 +0100

    refactor: remove data explorer scripts

--- dataworkspace/dataworkspace/apps/explorer/scripts/compile_assets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ./dataworkspace/apps/explorer/scripts/functions.sh
-
-run "npm run-script build"
-run "node -v"
--- dataworkspace/dataworkspace/apps/explorer/scripts/install_python_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ./dataworkspace/dataworkspace/apps/explorer/scripts/functions.sh
-
-run "pip install --upgrade pip"
-run 'pip install -r /tmp/requirements.txt'
