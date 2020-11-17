b863bc9944bf11453759309294c8eec1d4dc4032
Sekhar Panja <sekharpanja@gmail.com>
2020-11-17 08:14:21 +0000

    Separating out Celery worker into a docker container

--- dataworkspace/start-test.sh
+++ dataworkspace/start-test.sh
@@ -20,9 +20,10 @@ set -e
     mkdir -p /home/django/logs
 
     # Start nginx, proxy and application
-    echo "Starting celery beat, nginx, proxy and django application..."
-    parallel --will-cite --line-buffer --jobs 4 --halt now,done=1 ::: \
-        "celery beat   --app dataworkspace.cel.celery_app --pidfile /home/django/celerybeat.pid -S redbeat.RedBeatScheduler" \
+    echo "Starting celery, nginx, proxy and django application..."
+    parallel --will-cite --line-buffer --jobs 5 --halt now,done=1 ::: \
+        "celery worker --app dataworkspace.cel.celery_app --pool gevent --concurrency 150" \
+        "celery beat   --app dataworkspace.cel.celery_app --pidfile= -S redbeat.RedBeatScheduler" \
         "python3 -m start" \
         "PROXY_PORT='8001' UPSTREAM_ROOT='http://localhost:8002' python3 -m proxy" \
         "nginx -p /home/django"
