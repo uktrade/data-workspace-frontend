58061d1a26625455ee9fd9f2cfd766fac3875869
Nick Ross <nick.ir.ross@gmail.com>
2020-11-24 12:04:40 +0000

    feat: add rds parameter group terraform

---
+++ infra/rds_datasets.tf
@@ -0,0 +1,21 @@
+resource "aws_db_parameter_group" "default" {
+  name   = "postgres10-pgaudit"
+  description = "Postgres10 with pgaudit csvlogging enabled"
+  family = "postgres10"
+
+  parameter {
+    name  = "shared_preload_libraries"
+    value = "pgaudit"
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name  = "pgaudit.role"
+    value = "rds_pgaudit"
+  }
+
+  parameter {
+    name  = "log_destination"
+    value = "csvlog"
+  }
+}
