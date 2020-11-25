76ecce2a45c6e47943d57b18fec71102000bd5e2
Samuel Williams <samuelhwilliams@users.noreply.github.com>
2020-11-25 13:33:34 +0000

    Revert "move celery workers into separate docker container"

--- dataworkspace/start.sh
+++ dataworkspace/start.sh
@@ -20,8 +20,9 @@ set -e
     mkdir -p /home/django/logs
 
     # Start nginx, proxy and application
-    echo "Starting celery beat, nginx, proxy and django application..."
-    parallel --will-cite --line-buffer --jobs 4 --halt now,done=1 ::: \
+    echo "Starting nginx, proxy, django application, and celery..."
+    parallel --will-cite --line-buffer --jobs 5 --halt now,done=1 ::: \
+        "celery worker --app dataworkspace.cel.celery_app --pool gevent --concurrency 150" \
         "celery beat   --app dataworkspace.cel.celery_app --pidfile /home/django/celerybeat.pid -S redbeat.RedBeatScheduler" \
         "python3 -m start" \
         "PROXY_PORT='8001' UPSTREAM_ROOT='http://localhost:8002' python3 -m proxy" \
