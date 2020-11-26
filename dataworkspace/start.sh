40a4171a2b7dc184c6e47b87aaba6a2b5ae26be2
Sekhar Panja <sekharpanja@gmail.com>
2020-11-26 16:41:54 +0000

    remove celery worker from start.sh

--- dataworkspace/start.sh
+++ dataworkspace/start.sh
@@ -20,9 +20,8 @@ set -e
     mkdir -p /home/django/logs
 
     # Start nginx, proxy and application
-    echo "Starting nginx, proxy, django application, and celery..."
-    parallel --will-cite --line-buffer --jobs 5 --halt now,done=1 ::: \
-        "celery worker --app dataworkspace.cel.celery_app --pool gevent --concurrency 150" \
+    echo "Starting nginx, proxy, django application, and celery beat..."
+    parallel --will-cite --line-buffer --jobs 4 --halt now,done=1 ::: \
         "celery beat   --app dataworkspace.cel.celery_app --pidfile /home/django/celerybeat.pid -S redbeat.RedBeatScheduler" \
         "python3 -m start" \
         "PROXY_PORT='8001' UPSTREAM_ROOT='http://localhost:8002' python3 -m proxy" \
