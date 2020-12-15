9c78ba3eb25b75a88f8a897652815f048769478d
Nick Ross <nick.ir.ross@gmail.com>
2020-12-15 17:59:56 +0000

    fix: update regex for explorer queries

--- dataworkspace/dataworkspace/settings/base.py
+++ dataworkspace/dataworkspace/settings/base.py
@@ -564,5 +564,6 @@ PGAUDIT_IGNORE_STATEMENTS_RE = [
     r'SELECT version()',
     r'SELECT current_schema()',
     r"SELECT CAST\('.*?' AS VARCHAR\(\d+\)\) AS \w",
-    r"SELECT .*? FROM information_schema.tables WHERE table_schema not in \(.*?\) ORDER BY .*?",
+    r"SELECT .*? FROM information_schema.tables WHERE table_schema not in \(.*?\) ORDER BY .*",
+    r"^SELECT COUNT\(\*\) FROM \(.*?\) t$",
 ]
