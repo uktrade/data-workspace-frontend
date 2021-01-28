961d53729aefa75ba6960a00fc055da274a7d715
Samuel Williams <samuelhwilliams@gmail.com>
2021-01-28 16:57:15 +0000

    fix: update deprecated field name

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -32,7 +32,7 @@ resource "aws_vpc" "notebooks" {
 }
 
 resource "aws_flow_log" "notebooks" {
-  log_group_name = "${aws_cloudwatch_log_group.vpc_main_flow_log.name}"
+  log_destination = "${aws_cloudwatch_log_group.vpc_main_flow_log.name}"
   iam_role_arn   = "${aws_iam_role.vpc_notebooks_flow_log.arn}"
   vpc_id         = "${aws_vpc.notebooks.id}"
   traffic_type   = "ALL"
@@ -88,7 +88,7 @@ resource "aws_vpc_dhcp_options_association" "main" {
 }
 
 resource "aws_flow_log" "main" {
-  log_group_name = "${aws_cloudwatch_log_group.vpc_main_flow_log.name}"
+  log_destination = "${aws_cloudwatch_log_group.vpc_main_flow_log.name}"
   iam_role_arn   = "${aws_iam_role.vpc_main_flow_log.arn}"
   vpc_id         = "${aws_vpc.main.id}"
   traffic_type   = "ALL"
