b9f0f07975db259a83caff9d58e03290fabdcc07
Nick Ross <nick.ir.ross@gmail.com>
2021-02-16 13:25:42 +0000

    fix: remove unused template tag

--- dataworkspace/dataworkspace/apps/your_files/templatetags/your_files_tags.py
+++ dataworkspace/dataworkspace/apps/your_files/templatetags/your_files_tags.py
@@ -11,8 +11,3 @@ def remove_query_params(context, *args):
     return parse.urlencode(
         {key: value for key, value in context['request'].GET.items() if key not in args}
     )
-
-
-@register.simple_tag()
-def get_form_field(form, field_name):
-    return form.fields.get(field_name)
