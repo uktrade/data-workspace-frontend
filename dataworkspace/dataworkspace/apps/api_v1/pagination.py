6aca449d8f9fb71312ddc1a5070fa691f5ddde1f
Nick Ross <nick.ir.ross@gmail.com>
2021-03-11 17:05:45 +0000

    fix: allow client to specify page size

--- dataworkspace/dataworkspace/apps/api_v1/pagination.py
+++ dataworkspace/dataworkspace/apps/api_v1/pagination.py
@@ -3,3 +3,5 @@ from rest_framework.pagination import CursorPagination
 
 class TimestampCursorPagination(CursorPagination):
     ordering = ('timestamp', 'id')
+    page_size_query_param = 'page_size'
+    max_page_size = 10_000
