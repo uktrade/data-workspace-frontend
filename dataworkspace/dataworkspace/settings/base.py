dd10fa94bb6c1a3514b5e7ab9a6e6c47a9432b5d
Mark Higham <mark.higham@gmail.com>
2021-05-13 11:54:48 +0100

    removing changes to settings

--- dataworkspace/dataworkspace/settings/base.py
+++ dataworkspace/dataworkspace/settings/base.py
@@ -186,7 +186,7 @@ LOGGING = {
     'loggers': {
         'django': {'handlers': ['console'], 'level': 'INFO'},
         'app': {'handlers': ['console'], 'level': 'INFO'},
-        'dataworkspace': {'handlers': ['console'], 'level': 'DEBUG'},
+        'dataworkspace': {'handlers': ['console'], 'level': 'INFO'},
         'celery': {'handlers': ['console'], 'level': 'INFO', 'propagate': False},
     },
 }
