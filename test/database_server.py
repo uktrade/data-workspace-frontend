faa60655d48f6fdde9470e59638bcaba5cce597c
Mark Higham <mark.higham@gmail.com>
2021-05-28 16:07:43 +0100

    refactoring test server

--- test/database_server.py
+++ test/database_server.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 import sys
+from http import HTTPStatus
 
 import aiopg
 import psycopg2.sql
@@ -15,32 +16,46 @@ async def async_main():
         logger.setLevel(logging.DEBUG)
         logger.addHandler(stdout_handler)
 
-    async def handle_stop(_):
-        sys.exit()
-
-    async def handle_dataset(request):
+    async def handle_schema_dataset(request):
         database = request.match_info['database']
+        schema = request.match_info['schema']
         table = request.match_info['table']
 
-        logger.debug(database)
-
         dsn = os.environ[f'DATABASE_DSN__{database}']
+
+        sql = psycopg2.sql.SQL('SELECT * FROM {}.{}').format(
+            psycopg2.sql.Identifier(schema), psycopg2.sql.Identifier(table)
+        )
 
         async with aiopg.create_pool(dsn) as pool:
             async with pool.acquire() as conn:
                 async with conn.cursor() as cur:
-                    await cur.execute(
-                        psycopg2.sql.SQL('SELECT * FROM {}').format(
-                            psycopg2.sql.Identifier(table)
-                        )
-                    )
+                    await cur.execute(sql)
                     rows = [row[0] for row in await cur.fetchall()]
 
-        return web.json_response({'data': rows}, status=200)
+        return web.json_response(
+            {
+                'database': database,
+                'schema': schema,
+                'table': table,
+                'dsn': dsn,
+                'data': rows,
+            },
+            status=HTTPStatus.OK,
+            headers={},
+        )
+
+    async def handle_query_schema(request):
+        return web.json_response(
+            {}, status=200, headers={'from-upstream': 'upstream-header-value'}
+        )
 
     upstream = web.Application()
-    upstream.add_routes([web.post('/stop', handle_stop)])
-    upstream.add_routes([web.get('/{database}/{table}', handle_dataset)])
+    upstream.add_routes([web.get('/query_schema/{schema_name}', handle_query_schema)])
+
+    upstream.add_routes(
+        [web.get('/query_data/{database}/{schema}/{table}', handle_schema_dataset)]
+    )
 
     upstream_runner = web.AppRunner(upstream)
     await upstream_runner.setup()
