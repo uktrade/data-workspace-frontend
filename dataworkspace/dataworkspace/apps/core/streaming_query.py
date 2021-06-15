7f5ad9b6b60dd78da95c925bead559095dc19674
Mark Higham <mark.higham@gmail.com>
2021-06-15 15:13:45 +0100

    formatting "s as 's

--- dataworkspace/dataworkspace/apps/core/streaming_query.py
+++ dataworkspace/dataworkspace/apps/core/streaming_query.py
@@ -4,7 +4,7 @@ import queue
 
 import gevent
 from django.conf import settings
-from psycopg2 import connect
+from psycopg2 import connect, sql
 
 from dataworkspace.apps.core.utils import (
     StreamingHttpResponseWithoutDjangoDbConnection,
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 def new_streaming_query_response(
     user_email, database, filtered_query, unfiltered_query, filename, query_params=None
 ):
-    logger.info("hacking streaming_query_response")
+    logger.info('hacking streaming_query_response')
     batch_size = 1000
     query_timeout = 300 * 1000
 
@@ -49,7 +49,7 @@ def new_streaming_query_response(
                 rows = cur.fetchmany(batch_size)
 
                 if i == 0:  # Column names are not populated until the first row fetched
-                    logger.debug("write headers")
+                    logger.debug('write headers')
                     q.put(
                         csv_writer.writerow(
                             [column_desc[0] for column_desc in cur.description]
@@ -57,10 +57,10 @@ def new_streaming_query_response(
                     )
 
                 if not rows:
-                    logger.debug("no rows")
+                    logger.debug('no rows')
                     break
 
-                logger.debug("data")
+                logger.debug('data')
                 bytes_fetched = ''.join(
                     csv_writer.writerow(row) for row in rows
                 ).encode('utf-8')
@@ -74,20 +74,39 @@ def new_streaming_query_response(
         q.put(done)
 
     def get_all_columns_from_unfiltered(conn):
-        logger.debug("get_all_columns_from_unfiltered")
-        return []
+        logger.debug('get_all_columns_from_unfiltered')
+        columns_query = sql.SQL('SELECT * FROM ({query}) as data LIMIT 1').format(
+            query=unfiltered_query
+        )
+        with conn.cursor() as cur:
+            cur.execute(columns_query)
+            columns = [column_desc[0] for column_desc in cur.description]
+
+        return columns
 
     def get_row_count_from_unfiltered(conn):
-        query = "SELECT * FROM {"
+        logger.debug('get_row_count_from_unfiltered')
+        total_query = sql.SQL('SELECT COUNT(*) from ({query}) as data;').format(
+            query=unfiltered_query,
+        )
+
+        with conn.cursor() as cur:
+            cur.execute(total_query)
+
+            counts = cur.fetchone()
+            logger.info(counts)
+
+        return counts
 
     def steam_csv_and_calculate_totals():
         with connect(database_dsn(settings.DATABASES_DATA[database])) as conn:
             stream_query_as_csv_to_queue(conn)
 
             all_columns = get_all_columns_from_unfiltered(conn)
-            for x in range(0, 10):
-                gevent.sleep(1)
-                logger.info(f"in second query {x}")
+            counts = get_row_count_from_unfiltered(conn)
+
+        logger.debug(all_columns)
+        logger.debug(counts)
 
     def csv_iterator():
         # Listen for all data on the queue until we receive the done object
@@ -106,7 +125,15 @@ def new_streaming_query_response(
 
             q.task_done()
 
-    gevent.spawn(steam_csv_and_calculate_totals)
+    def exception_callback(g):
+        try:
+            g.get()
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            raise
+
+    g = gevent.spawn(steam_csv_and_calculate_totals)
+    g.link_exception(exception_callback)
 
     response = StreamingHttpResponseWithoutDjangoDbConnection(
         csv_iterator(), content_type='text/csv',
