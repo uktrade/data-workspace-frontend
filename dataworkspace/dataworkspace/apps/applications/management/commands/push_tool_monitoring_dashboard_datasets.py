575f7822c95a5b76ffcc899b9df53cc849b57c6d
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2021-07-04 16:27:45 +0100

    feature: add celery task to push tool usage datasets to geckoboard

---
+++ dataworkspace/dataworkspace/apps/applications/management/commands/push_tool_monitoring_dashboard_datasets.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from dataworkspace.apps.applications.utils import (
+    push_tool_monitoring_dashboard_datasets,
+)
+
+
+class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def handle(self, *args, **options):
+        push_tool_monitoring_dashboard_datasets()
+
+
+if __name__ == '__main__':
+    settings.configure()
+    Command().handle()
