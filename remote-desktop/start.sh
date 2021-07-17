1d42480c36dc6fb052b8da94c7758585877c59c9
Michal Charemza <michal@charemza.name>
2021-07-17 09:12:31 +0100

    refactor: slightly simpler remote desktop
    We can turn off the password, and pass the startup program via a command
    line argument

--- remote-desktop/start.sh
+++ remote-desktop/start.sh
@@ -2,18 +2,10 @@
 
 set -e
 
-mkdir -p /home/dw/.vnc
-
-# We can't put things in the home directory in the Dockerfile, since the
-# home directory is a bound volume, and its contents will disappear
-cp /xstartup /home/dw/.vnc/xstartup
-cp /passwd /home/dw/.vnc/passwd
-chmod 600 /home/dw/.vnc/passwd
-
 mkdir -p /home/dw/Desktop/
 cp /org.qgis.qgis.desktop /home/dw/Desktop/
 
-vncserver :1
+vncserver -SecurityTypes None -xstartup /usr/bin/lxsession :1
 
 # The webserver for static files that is build into websocketify occasionally
 # returns 500s. So we don't use it, and instead serve static files through nginx
