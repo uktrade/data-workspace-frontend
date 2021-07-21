beb9a33718c8d4c27c751fa6772a2891da398628
Mark Higham <mark.higham@gmail.com>
2021-07-21 15:26:33 +0100

    added commands to enable and disable all datagrids against CustomDataSetQueries

---
+++ dataworkspace/dataworkspace/apps/datasets/management/commands/disable_customdatasetquery_datagrid.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from dataworkspace.apps.datasets.models import CustomDatasetQuery
+
+
+class Command(BaseCommand):
+    help = "Enables ag-grid for CustomDatasetQuery"
+
+    def handle(self, *args, **options):
+        self.stdout.write("enabling ag-grid for DataCut datasets ...")
+
+        CustomDatasetQuery.objects.all().update(data_grid_enabled=False)
+
+        self.stdout.write(
+            self.style.SUCCESS("enabling ag-grid for CustomDatasetQuery (done)")
+        )
