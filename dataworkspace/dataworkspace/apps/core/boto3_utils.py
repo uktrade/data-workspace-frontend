d866f73c94982f9b35cc2781b1426a0e8ec1521c
Mark Higham <mark.higham@gmail.com>
2021-07-26 17:34:43 +0100

    using S3 endpoint if it is specified

--- dataworkspace/dataworkspace/apps/core/boto3_utils.py
+++ dataworkspace/dataworkspace/apps/core/boto3_utils.py
@@ -8,6 +8,6 @@ logger = logging.getLogger(__name__)
 
 def get_boto3_s3_client():
     endpoint_url = settings.AWS_S3_ENDPOINT_URL
-    logger.debug('AWS_S3_ENDPOINT_URL is %s', endpoint_url)
-    client = boto3.client('s3')
+    logger.debug("AWS_S3_ENDPOINT_URL is %s", endpoint_url)
+    client = boto3.client("s3", endpoint_url=endpoint_url)
     return client
