410cd4ed9aac3d8c37d0869dda8d8e668b0384d8
Mark Higham <mark.higham@gmail.com>
2021-08-09 15:21:35 +0100

    fix black formatting issue with migrations

--- dataworkspace/dataworkspace/apps/datasets/migrations/0085_remove_datasetvisualisation_thumbnail.py
+++ dataworkspace/dataworkspace/apps/datasets/migrations/0085_remove_datasetvisualisation_thumbnail.py
@@ -10,8 +10,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='datasetvisualisation',
-            name='thumbnail',
-        ),
+        migrations.RemoveField(model_name='datasetvisualisation', name='thumbnail',),
     ]
