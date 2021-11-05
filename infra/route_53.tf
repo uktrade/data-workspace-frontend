a6ed274f39b380f2a752b48ffb82bcc133668b78
Michal Charemza <michal@charemza.name>
2021-11-05 09:23:27 +0000

    refactor: remove unused registry components

--- infra/route_53.tf
+++ infra/route_53.tf
@@ -3,36 +3,6 @@ data "aws_route53_zone" "aws_route53_zone" {
   name = "${var.aws_route53_zone}"
 }
 
-resource "aws_route53_record" "registry" {
-  provider = "aws.route53"
-  zone_id = "${data.aws_route53_zone.aws_route53_zone.zone_id}"
-  name    = "${var.registry_internal_domain}"
-  type    = "A"
-
-  alias {
-    name                   = "${aws_alb.registry.dns_name}"
-    zone_id                = "${aws_alb.registry.zone_id}"
-    evaluate_target_health = false
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate" "registry" {
-  domain_name       = "${aws_route53_record.registry.name}"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "registry" {
-  certificate_arn = "${aws_acm_certificate.registry.arn}"
-}
-
 resource "aws_route53_record" "admin" {
   provider = "aws.route53"
   zone_id = "${data.aws_route53_zone.aws_route53_zone.zone_id}"
