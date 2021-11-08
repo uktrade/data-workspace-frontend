5cb70a1ab2a280a51fa9905bb5d8ad2910f00307
Mark Higham <mark.higham@gmail.com>
2021-11-08 17:31:02 +0000

    fix: excluding counts (1) from GTM data layer traffic

--- dataworkspace/dataworkspace/static/gtm-support.js
+++ dataworkspace/dataworkspace/static/gtm-support.js
@@ -1,32 +1,38 @@
-;
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-};
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-var GTMDatasetSearchSupport = function() {};
+var GTMDatasetSearchSupport = function () {};
 
 GTMDatasetSearchSupport.prototype.pushSearchEvent = function pushSearchEvent() {
-    if (typeof(dataLayer) !== 'undefined') {
-        var update = {
-            "event": "filter",
-            "searchTerms": document.getElementById('search').value,
-            "resultsReturned": parseInt(document.getElementById('search-results-count').textContent),
-        };
+  if (typeof dataLayer !== "undefined") {
+    var update = {
+      event: "filter",
+      searchTerms: document.getElementById("search").value,
+      resultsReturned: parseInt(
+        document.getElementById("search-results-count").textContent
+      ),
+    };
 
-        $("#live-search-form fieldset").map(
-            function () {
-                labels = [];
-                filter_id = $(this).attr('id').replace(/^id_/, "")
-                $(this).find('input[type=checkbox]:checked').map(
-                    function () {
-                        labels.push($("label[for='" + $(this).attr('id') + "']").text().trim());
-                    }
-                );
-                update['filterData' + capitalizeFirstLetter(filter_id)] = labels.sort().join(';')
-            }
-        )
+    $("#live-search-form fieldset").map(function () {
+      var labels = [];
+      var filterId = $(this).attr("id").replace(/^id_/, "");
+      $(this)
+        .find("input[type=checkbox]:checked")
+        .map(function () {
+          // the filter checkboxes have the number of results in brackets
+          // e.g. "Filter (1)"
+          // We remove the brackets and the number before sending to dataLayer
+          var labelText = $("label[for='" + $(this).attr("id") + "']").text();
+          var cleanText = labelText.replaceAll(/\([0-9]+\)/g, "");
+          labels.push(cleanText.trim());
+        });
 
-        dataLayer.push(update);
-    }
+      update["filterData" + capitalizeFirstLetter(filterId)] = labels
+        .sort()
+        .join(";");
+    });
+
+    dataLayer.push(update);
+  }
 };
-;
