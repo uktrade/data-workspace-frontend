cda2d7d1b42a3f49215b109c2e5569e476b0a1b4
Tom McDonagh <tom@Toms-MacBook-Pro-2.local>
2021-11-29 09:52:20 +0000

    adds user permissions and user sso to serializers, adds user permissions to serializer unit test, adds these new fields to views test

---
+++ dataworkspace/dataworkspace/tests/api_v1/accounts/test_serializers.py
@@ -0,0 +1,45 @@
+import pytest
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from rest_framework.serializers import Serializer
+
+from dataworkspace.tests import factories
+from dataworkspace.apps.api_v1.accounts.serializers import UserSerializer
+from dataworkspace.apps.applications.models import ApplicationInstance
+
+
+@pytest.mark.django_db
+class TestUserSerializer:
+    factory = factories.UserFactory
+
+    def test_permissions_subset_is_all_false(self):
+        user = self.factory()
+        serializer = UserSerializer(user)
+        serialized_data = serializer.data
+        user_permissions_subset = serialized_data["user_permissions_subset"]
+        assert user_permissions_subset == {
+            "develop_visualisations": False,
+            "access_quicksight": False,
+            "start_all_applications": False,
+            "access_appstream": False,
+        }
+
+    def test_permissions_subset_can_access_appstream(self):
+        user = self.factory()
+        permission = Permission.objects.create(
+            codename="access_appstream",
+            content_type=ContentType.objects.get_for_model(user),
+        )
+        user.user_permissions.add(permission)
+        serializer = UserSerializer(user)
+        serialized_data = serializer.data
+        user_permissions_subset = serialized_data["user_permissions_subset"]
+        assert user_permissions_subset == {
+            "develop_visualisations": False,
+            "access_quicksight": False,
+            "start_all_applications": False,
+            "access_appstream": True,
+        }
+
+    def test_user_sso(self):
+        user = self.factory()
