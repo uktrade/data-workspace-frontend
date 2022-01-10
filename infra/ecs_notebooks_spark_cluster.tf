2251fae2541dc2c462da6eb98729b499e521674a
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2022-01-10 17:37:31 +0000

    feature: add ecr repo and infra for launching a spark cluster as a tool Please enter the commit message for your changes. Lines starting

---
+++ infra/ecs_notebooks_spark_cluster.tf
@@ -0,0 +1,66 @@
+resource "aws_ecs_task_definition" "sparkcluster" {
+  family                = "${var.prefix}-sparkcluster"
+  container_definitions = "${data.template_file.sparkcluster_container_definitions.rendered}"
+  execution_role_arn    = "${aws_iam_role.notebook_task_execution.arn}"
+  network_mode          = "awsvpc"
+  cpu                   = "${local.notebook_container_cpu}"
+  memory                = "${local.notebook_container_memory}"
+  requires_compatibilities = ["FARGATE"]
+
+  volume {
+    name = "home_directory"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      "revision",
+    ]
+  }
+}
+
+data "external" "sparkcluster_current_tag" {
+  program = ["${path.module}/task_definition_tag.sh"]
+
+  query = {
+    task_family = "${var.prefix}-sparkcluster"
+    container_name = "${local.notebook_container_name}"
+  }
+}
+
+data "external" "sparkcluster_metrics_current_tag" {
+  program = ["${path.module}/task_definition_tag.sh"]
+
+  query = {
+    task_family = "${var.prefix}-sparkcluster"
+    container_name = "metrics"
+  }
+}
+
+data "external" "sparkcluster_s3sync_current_tag" {
+  program = ["${path.module}/task_definition_tag.sh"]
+
+  query = {
+    task_family = "${var.prefix}-sparkcluster"
+    container_name = "s3sync"
+  }
+}
+
+data "template_file" "sparkcluster_container_definitions" {
+  template = "${file("${path.module}/ecs_notebooks_notebook_container_definitions.json")}"
+
+  vars = {
+    container_image  = "${aws_ecr_repository.spark_cluster.repository_url}:master"
+    container_name   = "${local.notebook_container_name}"
+
+    log_group  = "${aws_cloudwatch_log_group.notebook.name}"
+    log_region = "${data.aws_region.aws_region.name}"
+
+    sentry_dsn = "${var.sentry_notebooks_dsn}"
+    sentry_environment = "${var.sentry_environment}"
+
+    metrics_container_image = "${aws_ecr_repository.metrics.repository_url}:master"
+    s3sync_container_image = "${aws_ecr_repository.s3sync.repository_url}:master"
+
+    home_directory = "/home/sparkcluster"
+  }
+}
