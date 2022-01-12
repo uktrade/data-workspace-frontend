a7b4bd5bfe4773be6f6eac8fb8333918be413eda
Nick Ross <nick.ross@digital.trade.gov.uk>
2022-01-12 13:10:49 +0000

    fix: update celery to fix CVE-2021-23727 (#1768)
    * fix: upgrade celery to fix CVE-2021-23727
    * fix: update order of arguments for celery commands
    * fix: upgrade celery redbeat

--- dataworkspace/start.sh
+++ dataworkspace/start.sh
@@ -22,7 +22,7 @@ set -e
     # Start nginx, proxy and application
     echo "Starting nginx, proxy, django application, and celery beat..."
     parallel --will-cite --line-buffer --jobs 4 --halt now,done=1 ::: \
-        "celery beat   --app dataworkspace.cel.celery_app --pidfile /home/django/celerybeat.pid -S redbeat.RedBeatScheduler" \
+        "celery --app dataworkspace.cel.celery_app beat --pidfile /home/django/celerybeat.pid -S redbeat.RedBeatScheduler" \
         "python3 -m start" \
         "PROXY_PORT='8001' UPSTREAM_ROOT='http://localhost:8002' python3 -m proxy" \
         "nginx -p /home/django"
