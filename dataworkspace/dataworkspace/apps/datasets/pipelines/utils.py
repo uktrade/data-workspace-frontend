22e57eb77b6af5995437e5a793d76a990e29d419
Nick Ross <nick.ir.ross@gmail.com>
2022-01-21 17:16:20 +0000

    fix: run pipeline on airflow by clicking the run button

--- dataworkspace/dataworkspace/apps/datasets/pipelines/utils.py
+++ dataworkspace/dataworkspace/apps/datasets/pipelines/utils.py
@@ -65,3 +65,30 @@ def delete_pipeline_from_dataflow(pipeline):
     )
     response.raise_for_status()
     return response.json()
+
+
+def run_pipeline(pipeline, run_by_user):
+    url = (
+        f"{settings.DATAFLOW_API_CONFIG['DATAFLOW_BASE_URL']}/api/experimental/"
+        f"dags/{pipeline.dag_id}/dag_runs"
+    )
+    method = "POST"
+    content_type = "application/json"
+    body = json.dumps(
+        {"replace_microseconds": "false", "conf": {"run_by_user": run_by_user.get_full_name()}}
+    )
+    header = Sender(
+        HAWK_CREDS,
+        url,
+        method.lower(),
+        content=body,
+        content_type=content_type,
+    ).request_header
+    response = requests.request(
+        method,
+        url,
+        data=body,
+        headers={"Authorization": header, "Content-Type": content_type},
+    )
+    response.raise_for_status()
+    return response.json()
