53ffb0d21035e34ef2d6a8a5eadac8bcbefa01b8
tommacca <tom.mcdonagh@trade.gov.uk>
2022-01-25 13:51:26 +0000

    rebase onto master

--- dataworkspace/dataworkspace/apps/datasets/pipelines/views.py
+++ dataworkspace/dataworkspace/apps/datasets/pipelines/views.py
@@ -1,3 +1,4 @@
+import requests, json
 import logging
 
 from django.contrib import messages
@@ -6,11 +7,14 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls.base import reverse_lazy
 from django.views.generic import DetailView
+from django.conf import settings
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse
 from requests import RequestException
+
+from mohawk import Sender
 
 from dataworkspace.apps.datasets.models import Pipeline
 from dataworkspace.apps.datasets.pipelines.forms import PipelineCreateForm, PipelineEditForm
@@ -154,9 +158,53 @@ class PipelineStopView(View, IsAdminMixin):
         return HttpResponseRedirect(reverse("pipelines:index"))
 
 
+
 class PipelineLogsDetailView(DetailView, UserPassesTestMixin):
     model = Pipeline
     template_name = "datasets/pipelines/logs.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["log"] = self._get_api_log()
+        return context
+
+    def _get_api_log(self):
+        derived_pipeline_object = self.object
+        derived_pipeline_table_name = derived_pipeline_object.table_name
+
+        # pipeline_name = "AppleCovid19MobilityTrendsPipeline"
+        pipeline_name = f"DerivedPipeline-{derived_pipeline_table_name}"
+
+        config = settings.DATAFLOW_API_CONFIG
+        url = (
+            f'{config["DATAFLOW_BASE_URL"]}/api/experimental/derived-dags/dag/'
+            f'{pipeline_name}/logs'
+        )
+
+        hawk_creds = {
+            "id": config["DATAFLOW_HAWK_ID"],
+            "key": config["DATAFLOW_HAWK_KEY"],
+            "algorithm": "sha256",
+        }
+        header = Sender(hawk_creds, url, "get", content="",
+                        content_type="").request_header
+
+        response = requests.get(url, headers={"Authorization": header,
+                                              "Content-Type": ""})
+        if response.status_code != 200:
+            return "Error"
+
+        try:
+            json_data = response.json()
+        except json.JSONDecodeError:
+            return response.text
+
+        processed_data = json_data
+
+        return processed_data
+
+
     def test_func(self):
         return self.request.user.is_superuser
+
+
