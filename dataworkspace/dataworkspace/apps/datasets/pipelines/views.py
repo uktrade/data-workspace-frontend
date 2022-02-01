b8cad50adbbb0d22298a4d26872e9b30b7b674e2
tommacca <tom.mcdonagh@trade.gov.uk>
2022-02-01 16:07:01 +0000

    removes old code introduced by rebase

--- dataworkspace/dataworkspace/apps/datasets/pipelines/views.py
+++ dataworkspace/dataworkspace/apps/datasets/pipelines/views.py
@@ -1,5 +1,4 @@
 import logging
-
 from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import HttpResponseRedirect
@@ -180,57 +179,3 @@ class PipelineLogsDetailView(DetailView, UserPassesTestMixin):
 
     def test_func(self):
         return self.request.user.is_superuser
-
-
-
-
-
-class PipelineLogsDetailView(DetailView, UserPassesTestMixin):
-    model = Pipeline
-    template_name = "datasets/pipelines/logs.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["log"] = self._get_api_log()
-        return context
-
-    def _get_api_log(self):
-        derived_pipeline_object = self.object
-        derived_pipeline_table_name = derived_pipeline_object.table_name
-
-        # pipeline_name = "AppleCovid19MobilityTrendsPipeline"
-        pipeline_name = f"DerivedPipeline-{derived_pipeline_table_name}"
-
-        config = settings.DATAFLOW_API_CONFIG
-        url = (
-            f'{config["DATAFLOW_BASE_URL"]}/api/experimental/derived-dags/dag/'
-            f'{pipeline_name}/logs'
-        )
-
-        hawk_creds = {
-            "id": config["DATAFLOW_HAWK_ID"],
-            "key": config["DATAFLOW_HAWK_KEY"],
-            "algorithm": "sha256",
-        }
-        header = Sender(hawk_creds, url, "get", content="",
-                        content_type="").request_header
-
-        response = requests.get(url, headers={"Authorization": header,
-                                              "Content-Type": ""})
-        if response.status_code != 200:
-            return "Error"
-
-        try:
-            json_data = response.json()
-        except json.JSONDecodeError:
-            return response.text
-
-        processed_data = json_data
-
-        return processed_data
-
-
-    def test_func(self):
-        return self.request.user.is_superuser
-
-
