665f27cec691625755d76af471ec5910cddb4f91
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2022-02-24 14:39:53 +0000

    feature: move tools out of repo to data-workspace-tools repo

--- tools/python-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install -r /root/requirements.txt
-python3 -m spacy download en
-python3 -m nltk.downloader -d /usr/local/share/nltk_data wordnet stopwords gutenberg
--- tools/rstudio/exploretiva/R/copy_explore_tiva.R
+++
@@ -1,5 +0,0 @@
-copy_explore_tiva <- function(path, ...) {
-  dir.create(path, recursive=TRUE, showWarnings=FALSE)
-  source <- system.file("templates", "explore_tiva.R", package="exploretiva")
-  file.copy(source, path)
-}
