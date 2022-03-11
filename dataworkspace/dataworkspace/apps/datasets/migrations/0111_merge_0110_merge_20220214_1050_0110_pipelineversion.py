c3caaf6a05e0333827f740fb4b6465e362a0869e
Nick Ross <nick.ir.ross@gmail.com>
2022-03-11 15:36:07 +0000

    fix: formating

--- dataworkspace/dataworkspace/apps/datasets/migrations/0111_merge_0110_merge_20220214_1050_0110_pipelineversion.py
+++ dataworkspace/dataworkspace/apps/datasets/migrations/0111_merge_0110_merge_20220214_1050_0110_pipelineversion.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('datasets', '0110_merge_20220214_1050'),
-        ('datasets', '0110_pipelineversion'),
+        ("datasets", "0110_merge_20220214_1050"),
+        ("datasets", "0110_pipelineversion"),
     ]
 
-    operations = [
-    ]
+    operations = []
