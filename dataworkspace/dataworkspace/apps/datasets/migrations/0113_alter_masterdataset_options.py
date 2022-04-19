5b0b203020813ba343d68c456ddc6dd7c443cb9d
Freya Krol <91138189+FREYA-KROL@users.noreply.github.com>
2022-04-19 13:40:38 +0100

    push migration to github

--- dataworkspace/dataworkspace/apps/datasets/migrations/0113_alter_masterdataset_options.py
+++ dataworkspace/dataworkspace/apps/datasets/migrations/0113_alter_masterdataset_options.py
@@ -6,12 +6,20 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('datasets', '0112_pendingauthorizedusers'),
+        ("datasets", "0112_pendingauthorizedusers"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='masterdataset',
-            options={'permissions': [('manage_unpublished_master_datasets', 'Manage (create, view, edit) unpublished source datasets')], 'verbose_name': 'Source Dataset'},
+            name="masterdataset",
+            options={
+                "permissions": [
+                    (
+                        "manage_unpublished_master_datasets",
+                        "Manage (create, view, edit) unpublished source datasets",
+                    )
+                ],
+                "verbose_name": "Source Dataset",
+            },
         ),
     ]
