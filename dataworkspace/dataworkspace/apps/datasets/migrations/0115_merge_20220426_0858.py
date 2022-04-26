548876ec0bffaad344f3cde8c767bbe0d69f7535
Freya Krol <91138189+FREYA-KROL@users.noreply.github.com>
2022-04-26 10:02:01 +0100

    fix linting

--- dataworkspace/dataworkspace/apps/datasets/migrations/0115_merge_20220426_0858.py
+++ dataworkspace/dataworkspace/apps/datasets/migrations/0115_merge_20220426_0858.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('datasets', '0113_alter_masterdataset_options'),
-        ('datasets', '0114_datasetchartbuilderchart_chart'),
+        ("datasets", "0113_alter_masterdataset_options"),
+        ("datasets", "0114_datasetchartbuilderchart_chart"),
     ]
 
-    operations = [
-    ]
+    operations = []
