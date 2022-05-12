c1001206b2a0e9a99831f4a43824b46ca195c7e9
Tash Boyse <tashboyse@Tashs-MacBook-Pro.local>
2022-05-12 10:54:07 +0100

    Refactor catalogue item function

--- dataworkspace/dataworkspace/apps/request_access/admin.py
+++ dataworkspace/dataworkspace/apps/request_access/admin.py
@@ -20,30 +20,27 @@ class AccessRequestAdmin(admin.ModelAdmin):
 
     @mark_safe
     def catalogue_item(self, obj):
-        try:
+        url = None
+        catalogue_item = None
+
+        if DataCutDataset.objects.filter(pk=obj.catalogue_item_id).exists():
             catalogue_item = DataCutDataset.objects.get(pk=obj.catalogue_item_id)
-        except DataCutDataset.DoesNotExist:
-            try:
-                catalogue_item = DataSet.objects.get(pk=obj.catalogue_item_id)
-            except DataSet.DoesNotExist:
-                try:
-                    catalogue_item = VisualisationCatalogueItem.objects.get(
-                        pk=obj.catalogue_item_id
-                    )
-                except VisualisationCatalogueItem.DoesNotExist:
-                    return None
-                else:
-                    url = reverse(
-                        "admin:datasets_visualisationcatalogueitem_change",
-                        args=(catalogue_item.id,),
-                    )
-            else:
-                url = reverse(
-                    "admin:datasets_masterdataset_change",
-                    args=(catalogue_item.id,),
-                )
-        else:
-            url = reverse("admin:datasets_datacutdataset_change", args=(catalogue_item.id,))
+            url = reverse(
+                "admin:datasets_datacutdataset_change",
+                args=(catalogue_item.id,),
+            )
+        elif DataSet.objects.filter(pk=obj.catalogue_item_id).exists():
+            catalogue_item = DataSet.objects.get(pk=obj.catalogue_item_id)
+            url = reverse(
+                "admin:datasets_masterdataset_change",
+                args=(catalogue_item.id,),
+            )
+        elif VisualisationCatalogueItem.objects.filter(pk=obj.catalogue_item_id).exists():
+            catalogue_item = VisualisationCatalogueItem.objects.get(pk=obj.catalogue_item_id)
+            url = reverse(
+                "admin:datasets_visualisationcatalogueitem_change",
+                args=(catalogue_item.id,),
+            )
 
         return '<a href="%s">%s</a>' % (url, catalogue_item)
 
