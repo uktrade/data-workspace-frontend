e3ba035a62de50f650314115e4f6cc4f5355b97e
Mark Higham <mark.higham@gmail.com>
2022-05-18 14:21:29 +0100

    fix: run priority celery tasks in different workers

--- dataworkspace/dataworkspace/cel.py
+++ dataworkspace/dataworkspace/cel.py
@@ -10,6 +10,11 @@ celery_app = Celery("dataworkspace")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
 
+if settings.USE_DEDICATED_CELERY_ROUTES:
+    # this *shouldn't* need to be applied directly but
+    # automatically configured via ^^ config_from_object
+    celery_app.conf.task_routes = settings.CELERY_ROUTES
+
 
 @after_setup_logger.connect
 def setup_loggers(*args, **kwargs):
