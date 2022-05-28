83754e3b307968bde352761257c857937c050daa
Michal Charemza <michal@charemza.name>
2022-05-28 12:58:20 +0100

    refactor: remove unused parameter

--- dataworkspace/dataworkspace/apps/datasets/search.py
+++ dataworkspace/dataworkspace/apps/datasets/search.py
@@ -188,7 +188,6 @@ def _get_visualisations_data_for_user_matching_query(visualisations: QuerySet, q
 def _get_datasets_data_for_user_matching_query(
     datasets: QuerySet,
     query,
-    data_type=None,
     user=None,
     id_field="id",
 ):
@@ -376,13 +375,13 @@ def _get_datasets_data_for_user_matching_query(
     )
 
 
-def _sorted_datasets_and_visualisations_matching_query_for_user(query, data_type, user, sort_by):
+def _sorted_datasets_and_visualisations_matching_query_for_user(query, user, sort_by):
     """
     Retrieves all master datasets, datacuts, reference datasets and visualisations (i.e. searchable items)
     and returns them, sorted by incoming sort field, default is desc(search_rank).
     """
     master_and_datacut_datasets = _get_datasets_data_for_user_matching_query(
-        DataSet.objects.live(), query, data_type, user=user, id_field="id"
+        DataSet.objects.live(), query, user=user, id_field="id"
     )
 
     reference_datasets = _get_datasets_data_for_user_matching_query(
@@ -413,7 +412,6 @@ def search_for_datasets(user, filters: SearchDatasetsFilters, matcher) -> tuple:
     all_datasets_visible_to_user_matching_query = (
         _sorted_datasets_and_visualisations_matching_query_for_user(
             query=filters.query,
-            data_type=filters.data_type,
             user=user,
             sort_by=filters.sort_type,
         )
--- dataworkspace/dataworkspace/tests/datasets/test_views.py
+++ dataworkspace/dataworkspace/tests/datasets/test_views.py
@@ -674,7 +674,7 @@ def test_datasets_and_visualisations_doesnt_return_duplicate_results(access_type
         assert len(datasets) == len(set(dataset["id"] for dataset in datasets))
 
         references = _get_datasets_data_for_user_matching_query(
-            ReferenceDataset.objects.live(), "", {}, user=u
+            ReferenceDataset.objects.live(), "", user=u
         )
         assert len(references) == len(set(reference["id"] for reference in references))
 
