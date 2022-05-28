4226b4f29ebf224c9e936dc99b568cdcb07cf118
Michal Charemza <michal@charemza.name>
2022-05-28 12:10:42 +0100

    refactor: remove unnecessary branching
    We always have a user

--- dataworkspace/dataworkspace/apps/datasets/search.py
+++ dataworkspace/dataworkspace/apps/datasets/search.py
@@ -28,7 +28,7 @@ from dataworkspace.apps.datasets.utils import (
 )
 
 
-def _get_visualisations_data_for_user_matching_query(visualisations: QuerySet, query, user=None):
+def _get_visualisations_data_for_user_matching_query(visualisations: QuerySet, query, user):
     """
     Filters the visualisation queryset for:
         1) visibility (whether the user can know if the visualisation exists)
@@ -39,8 +39,7 @@ def _get_visualisations_data_for_user_matching_query(visualisations: QuerySet, q
     """
     # Filter out visualisations that the user is not allowed to even know about.
     if not (
-        user
-        and user.has_perm(
+        user.has_perm(
             dataset_type_to_manage_unpublished_permission_codename(DataSetType.VISUALISATION)
         )
     ):
@@ -54,32 +53,28 @@ def _get_visualisations_data_for_user_matching_query(visualisations: QuerySet, q
         visualisations = visualisations.filter(search_vector=query)
 
     # Mark up whether the user can access the visualisation.
-    if user:
-        user_email_domain = user.email.split("@")[1]
-        access_filter = (
+    user_email_domain = user.email.split("@")[1]
+    access_filter = (
+        (
             (
-                (
-                    Q(
-                        user_access_type__in=[
-                            UserAccessType.REQUIRES_AUTHENTICATION,
-                            UserAccessType.OPEN,
-                        ]
-                    )
-                )
-                & (
-                    Q(visualisationuserpermission__user=user)
-                    | Q(visualisationuserpermission__isnull=True)
+                Q(
+                    user_access_type__in=[
+                        UserAccessType.REQUIRES_AUTHENTICATION,
+                        UserAccessType.OPEN,
+                    ]
                 )
             )
-            | Q(
-                user_access_type=UserAccessType.REQUIRES_AUTHORIZATION,
-                visualisationuserpermission__user=user,
+            & (
+                Q(visualisationuserpermission__user=user)
+                | Q(visualisationuserpermission__isnull=True)
             )
-            | Q(authorized_email_domains__contains=[user_email_domain])
         )
-
-    else:
-        access_filter = Q()
+        | Q(
+            user_access_type=UserAccessType.REQUIRES_AUTHORIZATION,
+            visualisationuserpermission__user=user,
+        )
+        | Q(authorized_email_domains__contains=[user_email_domain])
+    )
 
     visualisations = visualisations.annotate(
         _has_access=Case(
@@ -185,37 +180,36 @@ def _get_datasets_data_for_user_matching_query(
     datasets: QuerySet,
     query,
     id_field,
-    user=None,
+    user,
 ):
     is_reference_query = datasets.model is ReferenceDataset
 
     visibility_filter = Q(published=True)
 
-    if user:
-        if is_reference_query:
-            reference_type = DataSetType.REFERENCE
-            reference_perm = dataset_type_to_manage_unpublished_permission_codename(reference_type)
+    if is_reference_query:
+        reference_type = DataSetType.REFERENCE
+        reference_perm = dataset_type_to_manage_unpublished_permission_codename(reference_type)
 
-            if user.has_perm(reference_perm):
-                visibility_filter |= Q(published=False)
+        if user.has_perm(reference_perm):
+            visibility_filter |= Q(published=False)
 
-            datasets = datasets.annotate(
-                eligibility_criteria=Value(None, ArrayField(CharField(max_length=20)))
-            )
+        datasets = datasets.annotate(
+            eligibility_criteria=Value(None, ArrayField(CharField(max_length=20)))
+        )
 
-        if datasets.model is DataSet:
-            master_type, datacut_type = (
-                DataSetType.MASTER,
-                DataSetType.DATACUT,
-            )
-            master_perm = dataset_type_to_manage_unpublished_permission_codename(master_type)
-            datacut_perm = dataset_type_to_manage_unpublished_permission_codename(datacut_type)
+    if datasets.model is DataSet:
+        master_type, datacut_type = (
+            DataSetType.MASTER,
+            DataSetType.DATACUT,
+        )
+        master_perm = dataset_type_to_manage_unpublished_permission_codename(master_type)
+        datacut_perm = dataset_type_to_manage_unpublished_permission_codename(datacut_type)
 
-            if user.has_perm(master_perm):
-                visibility_filter |= Q(published=False, type=master_type)
+        if user.has_perm(master_perm):
+            visibility_filter |= Q(published=False, type=master_type)
 
-            if user.has_perm(datacut_perm):
-                visibility_filter |= Q(published=False, type=datacut_type)
+        if user.has_perm(datacut_perm):
+            visibility_filter |= Q(published=False, type=datacut_type)
 
     datasets = datasets.filter(visibility_filter)
 
@@ -232,7 +226,7 @@ def _get_datasets_data_for_user_matching_query(
     access_filter = Q()
     bookmark_filter = Q(referencedatasetbookmark__user=user)
 
-    if user and datasets.model is not ReferenceDataset:
+    if datasets.model is not ReferenceDataset:
         user_email_domain = user.email.split("@")[1]
         access_filter &= (
             Q(
