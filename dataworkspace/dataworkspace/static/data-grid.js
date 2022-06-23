66c49daa3d5d04a65e3b668606f5c456f065c4db
Nick Ross <nick.ir.ross@gmail.com>
2022-06-23 14:17:33 +0100

    fix: grid row values can be null or undefined

--- dataworkspace/dataworkspace/static/data-grid.js
+++ dataworkspace/dataworkspace/static/data-grid.js
@@ -203,8 +203,8 @@ function initDataGrid(columnConfig, dataEndpoint, downloadSegment, records, expo
     columnDefs: columnConfig,
     components: {
       loadingRenderer: function (params) {
-        if (params.value !== undefined) {
-          return params.valueFormatted !== undefined ? params.valueFormatted : params.value;
+        if (params.value != null) {
+          return params.valueFormatted != null ? params.valueFormatted : params.value;
         }
         return '<img src="/__django_static/assets/images/loading.gif">';
       },
