4b38346e279f6917dcef0bca0e8ba0fef0f78b7e
Nick Ross <nick.ir.ross@gmail.com>
2022-07-20 12:00:35 +0100

    feat: add enhanced table, apply to saved queries (#2085)
    - adds a new js script to turn an existing govuk styled table into a
      govuk styled grid
    - falls back to standard table if javascript is disabled
    - slight refactor of the data grid js to allow for sharing of certain
      functions
    - updates the saved queries page to merge the two tables and add sort
      functionality
    - change grid css to sass so we can add our own styles

--- dataworkspace/dataworkspace/static/data-grid.js
+++ dataworkspace/dataworkspace/static/data-grid.js
@@ -1,86 +1,3 @@
-function getCsrfToken() {
-  if (document.getElementsByName('csrfmiddlewaretoken').length > 0) {
-    return document.getElementsByName('csrfmiddlewaretoken')[0].value;
-  }
-  throw new Error('Unable to find CSRF token in the page');
-}
-
-function autoSizeColumns(columnApi) {
-  var allColumnIds = [];
-  columnApi.getAllColumns().forEach(function (column) {
-    allColumnIds.push(column.colId);
-  });
-  columnApi.autoSizeColumns(allColumnIds, false);
-}
-
-function compareDates(filterDate, cellDate) {
-  if (cellDate != null) {
-    filterDate = dayjs(filterDate);
-    cellDate = dayjs(cellDate);
-    if (cellDate < filterDate) {
-      return -1;
-    } else if (cellDate > filterDate) {
-      return 1;
-    }
-  }
-  return 0;
-}
-
-function objectToQueryString(obj) {
-  return '?' + Object.keys(obj).map(function(key) {
-    return key + '=' +  encodeURIComponent(obj[key]);
-  }).join('&');
-}
-
-function getBooleanFilterComponent() {
-  function BooleanFilterComponent() {}
-
-  BooleanFilterComponent.prototype.init = function (params) {
-    this.eGui = document.createElement('div');
-    this.eGui.className = 'ag-floating-filter-input';
-    this.eGui.innerHTML = '<div class="ag-labeled ag-label-align-left ag-text-field ag-input-field">' +
-      '<div class="ag-wrapper ag-input-wrapper ag-text-field-input-wrapper">' +
-      '<select><option></option><option value="1">True</option><option value="0">False</option></select></div></div>';
-
-    this.currentValue = null;
-    this.eFilterInput = this.eGui.querySelector('select');
-    this.eFilterInput.style.color = params.color;
-    var that = this;
-
-    function onSelectChanged() {
-      if (that.eFilterInput.value === '') {
-        params.parentFilterInstance(function (instance) {
-          instance.onFloatingFilterChanged(null, null);
-        });
-        return;
-      }
-
-      that.currentValue = Number(that.eFilterInput.value);
-      params.parentFilterInstance(function (instance) {
-        instance.onFloatingFilterChanged('equals', that.currentValue);
-      });
-    }
-
-    this.eFilterInput.addEventListener('input', onSelectChanged);
-
-  };
-
-  BooleanFilterComponent.prototype.onParentModelChanged = function (parentModel) {
-    if (!parentModel) {
-      this.eFilterInput.value = '';
-      this.currentValue = null;
-    } else {
-      this.eFilterInput.value = parentModel.filter + '';
-      this.currentValue = parentModel.filter;
-    }
-  };
-
-  BooleanFilterComponent.prototype.getGui = function () {
-    return this.eGui;
-  };
-
-  return BooleanFilterComponent;
-}
 
 function cleanFilters(filterModel, dataTypeMap) {
   var filters = filterModel != null ? filterModel : {};
@@ -167,7 +84,7 @@ function initDataGrid(columnConfig, dataEndpoint, downloadSegment, records, expo
     // (we do this here so it works for both client-side and server-side rendering)
     if (column.filter === 'agDateColumnFilter') {
       column.filterParams = {
-        comparator: compareDates
+        comparator: dateFilterComparator
       }
     }
 
