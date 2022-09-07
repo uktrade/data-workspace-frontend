2f66b49bba79150009159c8de41ae348f58a3ebd
Alex Ghoth <alex.ghoth@digital.trade.gov.uk>
2022-09-07 15:28:14 +0100

    better js if tag ids are not present

--- dataworkspace/dataworkspace/static/data-grid.js
+++ dataworkspace/dataworkspace/static/data-grid.js
@@ -173,13 +173,15 @@ function initDataGrid(columnConfig, dataEndpoint, downloadSegment, records, expo
               var response = JSON.parse(xhr.responseText);
               var rc = response.rowcount.count
               var dlimit = response.download_limit
+              const rowcount = document.getElementById('data-grid-rowcount')
+              const dl_count = document.getElementById('data-grid-download')
               if ((rc > dlimit) && (dlimit != null)) {
-                document.getElementById('data-grid-rowcount').innerText = dlimit+'+ rows';
-                document.getElementById('data-grid-download').innerText = 'Download this data ('+dlimit+' rows)';
+                if (rowcount) { rowcount.innerText = dlimit+'+ rows' }
+                if (dl_count) { dl_count.innerText = 'Download this data ('+dlimit+' rows)' }
               }
               else {
-                document.getElementById('data-grid-rowcount').innerText = rc+' rows';
-                document.getElementById('data-grid-download').innerText = 'Download this data ('+rc+' rows)';
+                if (rowcount) { rowcount.innerText = rc+' rows' }
+                if (dl_count) { dl_count.innerText = 'Download this data ('+rc+' rows)' }
               }
               params.successCallback(
                   response.records,
