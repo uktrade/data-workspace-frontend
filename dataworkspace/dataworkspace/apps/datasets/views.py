ba1af2b9b2ca3d5bcc8ba67f3c62929440361da8
Alex Ghoth <alex.ghoth@digital.trade.gov.uk>
2022-09-07 09:49:39 +0100

    return the actual dlimit (None/null) in the json

--- dataworkspace/dataworkspace/apps/datasets/views.py
+++ dataworkspace/dataworkspace/apps/datasets/views.py
@@ -1254,7 +1254,7 @@ class DataGridDataView(DetailView):
         return JsonResponse(
             {
                 "rowcount": rowcount[0],
-                "download_limit": download_limit,
+                "download_limit": source.data_grid_download_limit,
                 "records": records,
             }
         )
