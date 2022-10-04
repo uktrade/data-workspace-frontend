1f21f606ab75f581ad63e10f091864004ab6ba2b
Ross Miller <ross.miller@digital.trade.gov.uk>
2022-10-04 12:29:07 +0100

    Debug CI

--- dataworkspace/dataworkspace/settings/integration_tests.py
+++ dataworkspace/dataworkspace/settings/integration_tests.py
@@ -11,24 +11,6 @@ from helpdesk_client.interfaces import (
 
 import requests
 
-DEBUG = True
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "ecs": {"format": "%(asctime)s [%(levelname)s] [%(name)s] %(message)s"},
-        "verbose": {"format": "%(asctime)s [%(levelname)s] [%(name)s] %(message)s"},
-    },
-    "handlers": {"dev": {"class": "logging.StreamHandler", "formatter": "verbose"}},
-    "loggers": {
-        "app": {"handlers": ["dev"], "level": "DEBUG", "propagate": True},
-        "test": {"handlers": ["dev"], "level": "DEBUG", "propagate": True},
-        "dataworkspace": {"handlers": ["dev"], "level": "DEBUG", "propagate": True},
-        "celery": {"handlers": ["dev"], "level": "INFO", "propagate": False},
-    },
-}
-
 
 class HelpDeskTest(HelpDeskBase):
     def __init__(self, *args, **kwargs) -> None:
