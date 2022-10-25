83bd8004989333d7ab89e8c6721034e93f32df72
nboyse <natasha.boyse@digital.trade.gov.uk>
2022-10-25 17:47:48 +0100

    add extra fields for collection model and render context on basic page

--- dataworkspace/dataworkspace/apps/data_collections/models.py
+++ dataworkspace/dataworkspace/apps/data_collections/models.py
@@ -1,18 +1,26 @@
+from ckeditor.fields import RichTextField
+
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 
 from dataworkspace.apps.core.models import DeletableTimestampedUserModel
+from dataworkspace.apps.datasets.models import DataSet
 
 
 class Collection(DeletableTimestampedUserModel):
     id = models.AutoField(primary_key=True)
-    slug = models.SlugField(max_length=50, db_index=True, null=False, blank=False)
+    slug = models.SlugField(max_length=50, db_index=True, null=False, blank=False, unique=True)
     name = models.CharField(blank=False, null=False, max_length=128)
-    description = models.CharField(blank=False, null=False, max_length=256)
+    description = RichTextField(null=False, blank=False)
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True
     )
+
+    datasets = models.ManyToManyField(DataSet, related_name="datasets", blank=True)
+
+    published = models.BooleanField(default=False)
+    published_at = models.DateField(null=True, blank=True)
 
     def save(self, *args, **kwargs):
         slug = slugify(self.name)
--- dataworkspace/dataworkspace/apps/data_collections/views.py
+++ dataworkspace/dataworkspace/apps/data_collections/views.py
@@ -6,3 +6,9 @@ from dataworkspace.apps.data_collections.models import Collection
 class CollectionsDetailView(DetailView):
     def get_object(self, queryset=None):
         return Collection.objects.live().get(slug=self.kwargs["collections_slug"])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["source_object"] = self.get_object
+
+        return context
