eec442c3e78b3b2b9365033137fd0a93c91e251c
nboyse <natasha.boyse@digital.trade.gov.uk>
2022-10-26 10:53:08 +0100

    write test to check for django linebreaks template tag

---
+++ dataworkspace/dataworkspace/tests/data_collections/test_views.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+
+from dataworkspace.tests import factories
+
+
+def test_collection(client):
+    c = factories.CollectionFactory.create(slug="test-collections",
+                                           description="test collections description")
+    response = client.get(
+        reverse(
+            "data_collections:collections_view",
+            kwargs={"collections_slug": c.slug},
+        )
+    )
+    assert response.status_code == 200
+    assert "<p>test collections description</p>" in response.content.decode(response.charset)
