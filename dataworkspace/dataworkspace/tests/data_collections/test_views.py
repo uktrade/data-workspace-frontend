c029cbc7a0b3910c0c24feff9df513c9ed9c8a6c
TayyibDIT <tayyib.saddique@digital.trade.gov.uk>
2022-11-02 14:45:56 +0000

    adding test to delete visualisations in collection

--- dataworkspace/dataworkspace/tests/data_collections/test_views.py
+++ dataworkspace/dataworkspace/tests/data_collections/test_views.py
@@ -168,3 +168,69 @@ def test_authorised_user_attempting_delete_dataset_membership(user, other_user):
     assert "Datacut dataset has been removed from this collection" in response.content.decode(
         response.charset
     )
+
+def test_authorised_user_attempting_delete_visualisation_membership(user, other_user):
+    client_user = get_client(get_user_data(user))
+    client_other_user = get_client(get_user_data(other_user))
+
+    # Create the collection
+    c = factories.CollectionFactory.create(
+        name="test-collections",
+        description="test collections description",
+        published=True,
+        owner=user,
+    )
+
+    # Create a dataset and add it to the collection
+    dataset = factories.DatacutDataSetFactory(published=True, name="Visualisation")
+    c.datasets.add(dataset)
+    membership = c.dataset_collections.all()[0]
+    response = client_user.get(
+        reverse(
+            "data_collections:collections_view",
+            kwargs={"collections_id": c.id},
+        )
+    )
+    assert response.status_code == 200
+    assert ">Visualisation<" in response.content.decode(response.charset)
+
+    # Ensure that a user that isn't the owner can't remove it
+    response = client_other_user.post(
+        reverse(
+            "data_collections:collection_data_membership",
+            kwargs={"collections_id": c.id, "visualisation_membership_id": membership.id},
+        )
+    )
+    assert response.status_code == 404
+    response = client_user.get(
+        reverse(
+            "data_collections:collections_view",
+            kwargs={"collections_id": c.id},
+        )
+    )
+    assert response.status_code == 200
+    assert ">Visualisation<" in response.content.decode(response.charset)
+
+    # But the owner user can remove the dataset from the collection page
+    response = client_user.post(
+        reverse(
+            "data_collections:collection_data_membership",
+            kwargs={"collections_id": c.id, "visualisation_membership_id": membership.id},
+        )
+    )
+    assert response.status_code == 302
+    assert response["Location"] == reverse(
+        "data_collections:collections_view",
+        kwargs={"collections_id": c.id},
+    )
+    response = client_user.get(
+        reverse(
+            "data_collections:collections_view",
+            kwargs={"collections_id": c.id},
+        )
+    )
+    assert response.status_code == 200
+    assert ">Visualisations<" not in response.content.decode(response.charset)
+    assert "Visualisation has been removed from this collection" in response.content.decode(
+        response.charset
+    )
