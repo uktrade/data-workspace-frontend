d3cf502de0d102ef0b09124ef20d6e2c95575d44
Josef Smith <josef.smith@trade.gov.uk>
2023-01-25 16:19:00 +0000

    changed ordering of if statement for linting

--- dataworkspace/dataworkspace/apps/core/utils.py
+++ dataworkspace/dataworkspace/apps/core/utils.py
@@ -105,9 +105,9 @@ def new_private_database_credentials(
     force_create_for_databases: Tuple[str] = tuple(),
 ):
     db_team_roles = (
-        None
-        if dw_user == None
-        else [team.schema_name for team in Team.objects.filter(member=dw_user)]
+        [team.schema_name for team in Team.objects.filter(member=dw_user)]
+        if dw_user != None
+        else None
     )
     db_team_roles_set = set(db_team_roles)
     db_team_schemas = db_team_roles
