02377215274dc266122f6ae5eaf7403c41cc65d0
TayyibDIT <tayyib.saddique@digital.trade.gov.uk>
2023-03-06 16:51:15 +0000

    testing in circleci

--- dataworkspace/dataworkspace/apps/api_v1/datasets/serializers.py
+++ dataworkspace/dataworkspace/apps/api_v1/datasets/serializers.py
@@ -53,7 +53,7 @@ class CatalogueItemSerializer(serializers.Serializer):
     is_draft = serializers.BooleanField(source="draft")
     dictionary_published = serializers.BooleanField(source="dictionary")
     user_ids = serializers.ListField()
-    government_security_classification = serializers.ListField()
+    government_security_classification = serializers.IntegerField()
 
     def to_representation(self, instance):
         instance = super().to_representation(instance)
