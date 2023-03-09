a66ca163c2987e1621d05d84780f4e5ad5f5e757
nboyse <natasha.boyse@digital.trade.gov.uk>
2023-03-09 14:13:05 +0000

    add default visualisationlink to cover datasets etc.

--- dataworkspace/dataworkspace/apps/api_v1/datasets/views.py
+++ dataworkspace/dataworkspace/apps/api_v1/datasets/views.py
@@ -332,6 +332,7 @@ class CatalogueItemsInstanceViewSet(viewsets.ModelViewSet):
         "user_access_type",
         "authorized_email_domains",
         "user_ids",
+        "visualisation_link",
     ]
     queryset = (
         DataSet.objects.live()
