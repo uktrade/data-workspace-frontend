481314b05201a2a5ae5cde92889599e56c1e3198
Nick Ross <nick.ir.ross@gmail.com>
2023-03-20 15:58:19 +0000

    fix: allow for setting default num of celery db connections

--- dataworkspace/start-celery.sh
+++ dataworkspace/start-celery.sh
@@ -9,5 +9,5 @@ set -e
     parallel --will-cite --line-buffer --jobs 3 --halt now,done=1 ::: \
         "celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 150 --hostname spawner@%h -Q applications.spawner.spawn" \
         "celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 150 --hostname explorer@%h -Q explorer.tasks" \
-        "DEFAULT_DB_MAX_CONNS=60 celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 60 --hostname default@%h -X applications.spawner.spawn,explorer.tasks"
+        "DEFAULT_DB_MAX_CONNS=${CELERY_DEFAULT_DB_MAX_CONNS:=60} celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 60 --hostname default@%h -X applications.spawner.spawn,explorer.tasks"
 )
