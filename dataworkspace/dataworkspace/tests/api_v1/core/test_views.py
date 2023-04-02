01d68d16c44635f7388b2ee86fad717edd57e700
Teemerte Teccola <teemerte.teccola@digital.trade.gov.uk>
2023-04-02 22:14:55 +0100

    create id for test files

--- dataworkspace/dataworkspace/tests/api_v1/core/test_views.py
+++ dataworkspace/dataworkspace/tests/api_v1/core/test_views.py
@@ -203,7 +203,8 @@ class TestTeamsAPIView(BaseAPIViewTest):
         assert response.status_code == status.HTTP_200_OK
         assert response.json()["results"] == [
             {
-                "name": team1.name,
+                "name": team1.name[5:-1],
+                "id": team1.id,
                 "schema_name": team1.schema_name,
                 "members": [
                     user1.id,
@@ -212,6 +213,7 @@ class TestTeamsAPIView(BaseAPIViewTest):
             },
             {
                 "name": team2.name,
+                "id": team2.id[5:-1],
                 "schema_name": team2.schema_name,
                 "members": [
                     user3.id,
