2d228a74bc17a3cbca4d6c3ea76c2274d94a18b4
Nick Ross <nick.ir.ross@gmail.com>
2023-05-09 16:15:27 +0100

    fix: bump elasticache engine version to match aws (#2596)

--- infra/ecs_main_admin.tf
+++ infra/ecs_main_admin.tf
@@ -694,7 +694,7 @@ resource "aws_elasticache_cluster" "admin" {
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis5.0"
-  engine_version       = "5.0.3"
+  engine_version       = "5.0.6"
   port                 = 6379
   subnet_group_name    = "${aws_elasticache_subnet_group.admin.name}"
   security_group_ids   = ["${aws_security_group.admin_redis.id}"]
