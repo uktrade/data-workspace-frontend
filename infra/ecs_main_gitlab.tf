eb0f925f27191cb7bb8ad41e8a9d2679d358d772
Nick Ross <nick.ir.ross@gmail.com>
2023-05-09 14:46:07 +0100

    fix: bump elasticache engine version to match aws

--- infra/ecs_main_gitlab.tf
+++ infra/ecs_main_gitlab.tf
@@ -323,7 +323,7 @@ resource "aws_elasticache_cluster" "gitlab_redis" {
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis5.0"
-  engine_version       = "5.0.3"
+  engine_version       = "5.0.6"
   port                 = 6379
   subnet_group_name    = "${aws_elasticache_subnet_group.gitlab.name}"
   security_group_ids   = ["${aws_security_group.gitlab_redis.id}"]
