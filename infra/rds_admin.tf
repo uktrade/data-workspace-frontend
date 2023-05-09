1d6ac8a0394052da1cd91fd5f57a020ca469c930
Nick Ross <nick.ir.ross@gmail.com>
2023-05-09 14:24:37 +0100

    fix: enable performance insights

--- infra/rds_admin.tf
+++ infra/rds_admin.tf
@@ -21,6 +21,8 @@ resource "aws_db_instance" "admin" {
   vpc_security_group_ids = ["${aws_security_group.admin_db.id}"]
   db_subnet_group_name = "${aws_db_subnet_group.admin.name}"
 
+  performance_insights_enabled = true
+
   lifecycle {
     ignore_changes = [
       "snapshot_identifier",
