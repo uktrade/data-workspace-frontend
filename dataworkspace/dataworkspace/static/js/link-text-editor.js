e8a3670af499ca92024f8eedd96bec9bd7e1fb1e
Nick Ross <nick.ir.ross@gmail.com>
2023-05-24 10:24:54 +0100

    fix: collection notes
    - slight refactor of the rich text editors

--- dataworkspace/dataworkspace/static/js/link-text-editor.js
+++ dataworkspace/dataworkspace/static/js/link-text-editor.js
@@ -28,7 +28,7 @@ const linkRichTextConfig = {
 
 document.addEventListener("DOMContentLoaded", function () {
   const textAreas = Array.prototype.slice.call(
-    document.querySelectorAll("textarea[data-type=rich-text-editor][id=id_restrictions_on_usage]")
+    document.querySelectorAll("textarea[data-type=rich-link-text-editor]")
   );
   for (let i = 0; i < textAreas.length; ++i) {
     ClassicEditor.create(textAreas[i], linkRichTextConfig).catch((error) => {
