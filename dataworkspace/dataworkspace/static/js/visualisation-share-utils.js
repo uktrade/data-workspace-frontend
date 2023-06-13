05538846c002e1d87ba8ecec1025966ab4040b80
Nick Ross <nick.ir.ross@gmail.com>
2023-06-13 14:28:06 +0100

    fix: init js once dom has loaded (#2657)

--- dataworkspace/dataworkspace/static/js/visualisation-share-utils.js
+++ dataworkspace/dataworkspace/static/js/visualisation-share-utils.js
@@ -1,49 +1,51 @@
-document
-  .getElementById("shareDashboardPopUp")
-  .addEventListener("click", function (e) {
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("shareDashboardPopUp")
+    .addEventListener("click", function (e) {
+      e.stopPropagation();
+      e.preventDefault();
+      const copyButton = document.getElementById("copy-to-clipboard");
+      copyButton.innerText = "Copy link to dashboard";
+      copyButton.classList.remove("govuk-button--disabled");
+      copyButton.disabled = false;
+      document.getElementById("popup").showModal();
+    });
+
+  document.getElementById("closePopUp").addEventListener("click", function (e) {
     e.stopPropagation();
     e.preventDefault();
-    const copyButton = document.getElementById("copy-to-clipboard");
-    copyButton.innerText = "Copy link to dashboard";
-    copyButton.classList.remove("govuk-button--disabled");
-    copyButton.disabled = false;
-    document.getElementById("popup").showModal();
+    document.getElementById("popup").close();
   });
 
-document.getElementById("closePopUp").addEventListener("click", function (e) {
-  e.stopPropagation();
-  e.preventDefault();
-  document.getElementById("popup").close();
+  document
+    .getElementById("copy-to-clipboard")
+    .addEventListener("click", function (e) {
+      e.stopPropagation();
+      e.preventDefault();
+      const textAreaInput = document.getElementById("share-dashboard").value;
+      const copyButton = document.getElementById("copy-to-clipboard");
+
+      if (navigator.clipboard) {
+        navigator.clipboard
+          .writeText(textAreaInput)
+          .then(() => {
+            copyButton.innerText = "Link copied";
+            copyButton.classList.add("govuk-button--disabled");
+            copyButton.disabled = true;
+          })
+          .catch((error) => {
+            console.error("failed to copy link", error);
+          });
+      } else {
+        const textarea = document.createElement("textarea");
+        textarea.value = textAreaInput;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textarea);
+        copyButton.innerText = "Link copied";
+        copyButton.classList.add("govuk-button--disabled");
+        copyButton.disabled = true;
+      }
+    });
 });
-
-document
-  .getElementById("copy-to-clipboard")
-  .addEventListener("click", function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    const textAreaInput = document.getElementById("share-dashboard").value;
-    const copyButton = document.getElementById("copy-to-clipboard");
-
-    if (navigator.clipboard) {
-      navigator.clipboard
-        .writeText(textAreaInput)
-        .then(() => {
-          copyButton.innerText = "Link copied";
-          copyButton.classList.add("govuk-button--disabled");
-          copyButton.disabled = true;
-        })
-        .catch((error) => {
-          console.error("failed to copy link", error);
-        });
-    } else {
-      const textarea = document.createElement("textarea");
-      textarea.value = textAreaInput;
-      document.body.appendChild(textarea);
-      textarea.select();
-      document.execCommand("copy");
-      document.body.removeChild(textarea);
-      copyButton.innerText = "Link copied";
-      copyButton.classList.add("govuk-button--disabled");
-      copyButton.disabled = true;
-    }
-  });
