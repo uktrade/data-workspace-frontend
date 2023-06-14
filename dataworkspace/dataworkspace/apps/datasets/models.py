1234a8c3742fe023e0b72e304704f22a8998f6d9
nboyse <natasha.boyse@digital.trade.gov.uk>
2023-06-14 15:35:43 +0100

    fixing tuple concat issue

--- dataworkspace/dataworkspace/apps/datasets/models.py
+++ dataworkspace/dataworkspace/apps/datasets/models.py
@@ -457,7 +457,7 @@ class DataSet(DeletableTimestampedUserModel):
             in (
                 self.information_asset_owner_id,
                 self.information_asset_manager_id,
-            ) + self.data_catalogue_editors.values_list("id", flat=True)
+            ) + tuple(self.data_catalogue_editors.values_list("id", flat=True))
         )
 
     def user_has_bookmarked(self, user):
@@ -2616,7 +2616,7 @@ class VisualisationCatalogueItem(DeletableTimestampedUserModel):
             in (
                 self.information_asset_owner_id,
                 self.information_asset_manager_id,
-            ) + self.data_catalogue_editors.values_list("id", flat=True)
+            ) + tuple(self.data_catalogue_editors.values_list("id", flat=True))
         )
 
     def user_has_bookmarked(self, user):
