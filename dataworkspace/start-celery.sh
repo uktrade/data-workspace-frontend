4be3842802a4bf74e2f290b66db9f8dddfe2c0b9
Nick Ross <nick.ir.ross@gmail.com>
2023-06-23 16:33:21 +0100

    fix: max celery db conns (#2673)

--- dataworkspace/start-celery.sh
+++ dataworkspace/start-celery.sh
@@ -5,9 +5,10 @@ set -e
 (
     cd "$(dirname "$0")"
 
-    echo "starting celery..."
+    DEFAULT_DB_MAX_CONNS=${CELERY_DEFAULT_DB_MAX_CONNS:=60};
+    echo "starting celery with $DEFAULT_DB_MAX_CONNS DB connections..."
     parallel --will-cite --line-buffer --jobs 3 --halt now,done=1 ::: \
         "celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 150 --hostname spawner@%h -Q applications.spawner.spawn" \
         "celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 150 --hostname explorer@%h -Q explorer.tasks" \
-        "DEFAULT_DB_MAX_CONNS=${CELERY_DEFAULT_DB_MAX_CONNS:=60} celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 60 --hostname default@%h -X applications.spawner.spawn,explorer.tasks"
+        "celery --app dataworkspace.cel.celery_app worker --pool gevent --prefetch-multiplier=1 --concurrency 60 --hostname default@%h -X applications.spawner.spawn,explorer.tasks"
 )
