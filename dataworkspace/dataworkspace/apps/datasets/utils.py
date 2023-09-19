d606f88ba98b8f7b891ebc8b9d043ddd7047b57a
Teemerte Teccola <94635933+Teccolat@users.noreply.github.com>
2023-09-19 16:57:24 +0100

    fix unique inner join claus (#2774)
    where there is a duplicate inner joins.

--- dataworkspace/dataworkspace/apps/datasets/utils.py
+++ dataworkspace/dataworkspace/apps/datasets/utils.py
@@ -164,12 +164,17 @@ def get_sql_snippet_for_reference_dataset(schema, dataset, limit):
     base_table = dataset.table_name
     fields = f"{base_table}.*"
     joins = ""
+    unique_tables = []
     for col_def in col_defs:
         fields = fields + ", " + f'{col_def["table"]}.{col_def["ext_field"]}'
-        joins = (
-            joins
-            + f' INNER JOIN "{schema}"."{col_def["table"]}" ON {base_table}.{col_def["field"]}_id = {col_def["table"]}.id'
-        )
+        if col_def["table"] not in unique_tables:
+            unique_tables.append(col_def["table"])
+            joins = (
+                joins
+                + f""" INNER JOIN "{schema}"."{col_def["table"]}" \
+                ON {base_table}.{col_def["field"]}_id = {col_def["table"]}.id"""
+            )
+
     sql = f'SELECT {fields} FROM "{schema}"."{base_table}"{joins} LIMIT {limit}'
     formated_sql = sqlparse.format(sql, reindent=True, keyword_case="upper")
     return formated_sql
