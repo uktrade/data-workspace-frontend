293d17e5c8098c84534ec76ed3db11b127acc303
Ian Leggett <contactleggett@gmail.com>
2023-10-23 16:17:41 +0100

    chore: remove duplication and update typing

---
+++ dataworkspace/dataworkspace/static/js/react/components/FetchDataContainer/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+import { fetchDataUsage } from '../../services';
+import { DataType } from '../../services';
+
+type FetchDataContainerProps = {
+  id: string;
+  dataType: DataType;
+  children: ({
+    data,
+    loading,
+    error
+  }: {
+    data: { title: string; value: number }[];
+    loading: boolean;
+    error: string | null;
+  }) => React.ReactNode;
+};
+
+const FetchDataContainer = ({
+  id,
+  dataType,
+  children
+}: FetchDataContainerProps) => {
+  const [data, setData] = useState<{ title: string; value: number }[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null | string>(null);
+
+  const childProps = {
+    data,
+    loading,
+    error
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetchDataUsage(dataType, id);
+      response instanceof Error
+        ? setError(response.message)
+        : setData(response);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return children(childProps);
+};
+
+export default FetchDataContainer;
--- dataworkspace/dataworkspace/static/js/react/services.ts
+++ dataworkspace/dataworkspace/static/js/react/services.ts
@@ -1,15 +1,16 @@
 import { API_BASE_URL, DATA_USAGE_KEYS } from './constants';
 
 type DataUsageKeys = keyof typeof DATA_USAGE_KEYS;
+type DataUsageValues = (typeof DATA_USAGE_KEYS)[keyof typeof DATA_USAGE_KEYS];
 
 type DataUsage = Record<string, number>;
 
-type TransformedDataUsageResponse = {
-  title: string;
+export type TransformedDataUsageResponse = {
+  title: DataUsageValues;
   value: number;
 };
 
-type DataType = 'datasets' | 'visualisation' | 'reference';
+export type DataType = 'datasets' | 'visualisation' | 'reference';
 
 export const transformDataUsageResponse = (
   response: DataUsage
@@ -21,8 +22,21 @@ export const transformDataUsageResponse = (
     })
   );
 
-export const fetchDataUsage = async (dataType: DataType, id: string) => {
-  const response = await fetch(`/${API_BASE_URL}/${dataType}/${id}/stats/`);
-  const dataUsage = await response.json();
-  return transformDataUsageResponse(dataUsage);
+export const fetchDataUsage = async (
+  dataType: DataType,
+  id: string
+): Promise<TransformedDataUsageResponse[] | Error> => {
+  try {
+    const response = await fetch(`/${API_BASE_URL}/${dataType}/${id}/stats/`);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    } else {
+      return transformDataUsageResponse(data);
+    }
+  } catch (error: unknown) {
+    return error instanceof Error
+      ? error
+      : new Error('Error: something has gone wrong.');
+  }
 };
