cdf0e26ab3a7c6e002f98050f0461ea71e84702c
Ian Leggett <contactleggett@gmail.com>
2023-10-25 12:14:44 +0100

    chore: remove data usage footer message

--- dataworkspace/dataworkspace/static/js/react/modules/reference-dataset/container.tsx
+++ dataworkspace/dataworkspace/static/js/react/modules/reference-dataset/container.tsx
@@ -5,12 +5,7 @@ import { fetchDataUsage } from '../../services';
 
 const Container = ({ id }: { id: string }): React.ReactNode => (
   <FetchDataContainer fetchApi={() => fetchDataUsage('reference', id)}>
-    {({ data }) => (
-      <DataDisplay
-        data={data}
-        footerNote={<a href="/">Find out more about the metrics above</a>}
-      />
-    )}
+    {({ data }) => <DataDisplay data={data} />}
   </FetchDataContainer>
 );
 
