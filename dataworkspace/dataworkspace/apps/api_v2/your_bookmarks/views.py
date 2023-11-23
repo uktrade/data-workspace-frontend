fc8584be3a2ed476b3317f8a24fb309c52b1b158
Oliver Weare <oliver_weare@protonmail.com>
2023-11-23 12:25:28 +0000

    feat: your bookmarks api filtering and ordering

--- dataworkspace/dataworkspace/apps/api_v2/your_bookmarks/views.py
+++ dataworkspace/dataworkspace/apps/api_v2/your_bookmarks/views.py
@@ -1,16 +1,31 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from dataworkspace.apps.api_v2.datasets.serializers import DatasetSerializer
 from dataworkspace.apps.datasets.models import DataSet
+from dataworkspace.apps.eventlog.models import EventLog
+
+
+class YourBookmarkPageNumberPagination(PageNumberPagination):
+    ordering = ("-timestamp", "id")
+    page_size_query_param = "page_size"
+    max_page_size = 10_000
 
 
 class YourBookmarksViewSet(viewsets.ModelViewSet):
     queryset = DataSet.objects.all()
     serializer_class = DatasetSerializer
+    pagination_class = YourBookmarkPageNumberPagination
     authentication_classes = [SessionAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return super().get_queryset().filter(user_has_bookmarked=self.request.user)
+        return (
+            super()
+            .get_queryset()
+            .filter(
+                user_has_bookmarked=self.request.user, event_type=EventLog.TYPE_DATASET_BOOKMARKED
+            )
+        )
