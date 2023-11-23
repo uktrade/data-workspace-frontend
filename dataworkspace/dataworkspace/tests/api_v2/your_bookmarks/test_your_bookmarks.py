aaa116151b5c8d890045fe9ab759ee7e2cc0d266
Oliver Weare <oliver_weare@protonmail.com>
2023-11-23 19:27:47 +0000

    feat: api tests

--- dataworkspace/dataworkspace/tests/api_v2/your_bookmarks/test_your_bookmarks.py
+++ dataworkspace/dataworkspace/tests/api_v2/your_bookmarks/test_your_bookmarks.py
@@ -1,49 +1,42 @@
-import pytest
-from django.urls import reverse
-from rest_framework import status
-
-from dataworkspace.tests import factories
+from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from dataworkspace.apps.datasets.models import DataSet
 from dataworkspace.apps.eventlog.models import EventLog
 
 
-@pytest.mark.django_db
-def test_unauthenticated_your_bookmarks(unauthenticated_client):
-    response = unauthenticated_client.get(reverse("api-v2:your_bookmarks:dataset-list"))
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+class YourBookmarksViewSetTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.client.force_authenticate(user=self.user)
 
+    def test_get_bookmarks(self):
+        # Create a DataSet with bookmark for the user
+        dataset = DataSet.objects.create(name="Test Dataset")
+        EventLog.objects.create(
+            user_has_bookmarked=self.user,
+            event_type=EventLog.TYPE_DATASET_BOOKMARKED,
+            content_object=dataset,
+        )
 
-@pytest.mark.django_db
-def test_ordering_and_filtering_bookmarked_items(client, user):
-    client.force_login(user)
-    user_event1 = factories.EventLogFactory(
-        user=user,
-        event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.DataSetBookmarkFactory.create(),
-    )
-    user_event2 = factories.EventLogFactory(
-        user=user,
-        event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.DataSetBookmarkFactory.create(),
-    )
-    user_event3 = factories.EventLogFactory(
-        user=user,
-        event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.DataSetBookmarkFactory.create(),
-    )
-    factories.EventLogFactory(
-        event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.DataSetBookmarkFactory.create(),
-    )
-    factories.EventLogFactory(
-        user=user,
-        event_type=EventLog.TYPE_DATASET_FIND_FORM_QUERY,
-        related_object=factories.DataSetBookmarkFactory.create(),
-    )
+        # Make a GET request to the viewset
+        response = self.client.get("/your-bookmarks-endpoint/")
 
-    response = client.get(reverse("api-v2:your_bookmarks:dataset-list"))
-    bookmarks = response.json()
-    assert len(bookmarks["results"]) == 3
-    assert bookmarks["results"][0]["id"] == user_event3.id
-    assert bookmarks["results"][1]["id"] == user_event2.id
-    assert bookmarks["results"][2]["id"] == user_event1.id
-    assert response.status_code == status.HTTP_200_OK
+        # Check that the response has a 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the serialized data in the response matches your expectations
+        self.assertEqual(response.data["results"][0]["name"], "Test Dataset")
+
+    def test_empty_bookmarks(self):
+        # Make a GET request to the viewset
+        response = self.client.get("/your-bookmarks-endpoint/")
+
+        # Check that the response has a 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the response contains an empty list of results
+        self.assertEqual(response.data["results"], [])
+
+    # You can add more test cases for other actions (create, update, delete) if needed
