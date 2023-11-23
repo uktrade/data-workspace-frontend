df99d428e55734a39e00d361c0930d21ba381579
Oliver Weare <oliver_weare@protonmail.com>
2023-11-23 17:46:42 +0000

    feat: api tests

--- dataworkspace/dataworkspace/tests/api_v2/your_bookmarks/test_your_bookmarks.py
+++ dataworkspace/dataworkspace/tests/api_v2/your_bookmarks/test_your_bookmarks.py
@@ -18,21 +18,21 @@ def test_ordering_and_filtering_bookmarked_items(client, user):
     user_event1 = factories.EventLogFactory(
         user=user,
         event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.DatasetFactory.create(),
+        related_object=factories.ReferenceDatasetFactory.create(),
     )
     user_event2 = factories.EventLogFactory(
         user=user,
         event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.DatasetFactory.create(),
+        related_object=factories.ReferenceDatasetFactory.create(),
     )
     user_event3 = factories.EventLogFactory(
         user=user,
         event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.DatasetFactory.create(),
+        related_object=factories.ReferenceDatasetFactory.create(),
     )
     factories.EventLogFactory(
         event_type=EventLog.TYPE_DATASET_BOOKMARKED,
-        related_object=factories.ReferenceDatasetFactory.create(),
+        related_object=factories.ReferenceReferenceDatasetFactory.create(),
     )
 
     response = client.get(reverse("api-v2:your_bookmarks"))
