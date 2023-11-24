bc8689974c04c8350a97ff1e761ee2f0d6b096a5
Oliver Weare <oliver_weare@protonmail.com>
2023-11-24 09:17:49 +0000

    feat: api tests

--- dataworkspace/dataworkspace/tests/api_v2/your_bookmarks/test_your_bookmarks.py
+++ dataworkspace/dataworkspace/tests/api_v2/your_bookmarks/test_your_bookmarks.py
@@ -13,6 +13,13 @@ def test_unauthenticated_your_bookmarks(unauthenticated_client):
 
 
 @pytest.mark.django_db
+def test_authenticated_user_empty_bookmarks(client, user):
+    client.force_login(user)
+    response = client.get(reverse("api-v2:your_bookmarks:dataset-list"))
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
 def test_ordering_and_filtering_bookmarked_items(client, user):
     client.force_login(user)
     user_event1 = factories.EventLogFactory(
@@ -36,13 +43,6 @@ def test_ordering_and_filtering_bookmarked_items(client, user):
     assert your_bookmarks["results"][0]["id"] == user_event3.id
     assert your_bookmarks["results"][1]["id"] == user_event2.id
     assert your_bookmarks["results"][2]["id"] == user_event1.id
-    assert response.status_code == status.HTTP_200_OK
-
-
-def test_authenticated_user_empty_bookmarks(client, user):
-    client.force_login(user)
-
-    response = client.get(reverse("api-v2:your_bookmarks:dataset-list"))
     assert response.status_code == status.HTTP_200_OK
 
     response_data = response.json()
