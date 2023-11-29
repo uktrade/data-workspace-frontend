29ff04647de9175670bdfc64bdc1f364a50d8d64
Nick Ross <nick.ir.ross@gmail.com>
2023-11-29 09:01:35 +0000

    fix: mark a task as stopped if it doesn't have an arn

--- dataworkspace/dataworkspace/apps/applications/spawner.py
+++ dataworkspace/dataworkspace/apps/applications/spawner.py
@@ -548,7 +548,20 @@ class FargateSpawner:
         application_instance = ApplicationInstance.objects.get(id=application_instance_id)
         options = json.loads(application_instance.spawner_application_template_options)
         cluster_name = options["CLUSTER_NAME"]
-        task_arn = json.loads(application_instance.spawner_application_instance_id)["task_arn"]
+        # If there is no task arn for the application mark it as stopped
+        try:
+            task_arn = json.loads(application_instance.spawner_application_instance_id)["task_arn"]
+        except KeyError:
+            if application_instance.state != "STOPPED":
+                application_instance.state = "STOPPED"
+                application_instance.single_running_or_spawning_integrity = str(
+                    application_instance.id
+                )
+                application_instance.save(
+                    update_fields=["state", "single_running_or_spawning_integrity"]
+                )
+            return
+
         _fargate_task_stop(cluster_name, task_arn)
 
         sleep_time = 1
