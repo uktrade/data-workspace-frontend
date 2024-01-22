9c0453befbdd6e23cc6e03dcaed3ff975c413d4c
Caitlin Barnard <caitlin@madetech.com>
2024-01-22 16:34:20 +0000

    Fix import

--- dataworkspace/dataworkspace/apps/maintenance/views.py
+++ dataworkspace/dataworkspace/apps/maintenance/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 
-from dataworkspace.dataworkspace.apps.maintenance.models import MaintenanceSettings
+from dataworkspace.apps.maintenance.models import MaintenanceSettings
 from dataworkspace.apps.maintenance.serializers import MaintenanceSettingsSerializer
 
 
