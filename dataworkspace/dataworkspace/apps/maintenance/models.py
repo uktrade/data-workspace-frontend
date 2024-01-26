0ecfa27da7f5d4eb92abbe5ae4fbd6e3ad79909e
Caitlin Barnard <caitlin@madetech.com>
2024-01-26 16:28:05 +0000

    linting

--- dataworkspace/dataworkspace/apps/maintenance/models.py
+++ dataworkspace/dataworkspace/apps/maintenance/models.py
@@ -3,12 +3,14 @@ import bleach
 
 
 class MaintenanceSettings(models.Model):
-    maintenance_text = models.CharField(help_text="This is the main content of the maintenance page. You can use HTML &lt;br&gt; tags to format the text.")
+    maintenance_text = models.CharField(
+        help_text="This is the main content of the maintenance page. You can use HTML &lt;br&gt; tags to format the text."
+    )
     maintenance_toggle = models.BooleanField()
     contact_email = models.EmailField()
 
     def save(self, *args, **kwargs):
-        self.maintenance_text = bleach.clean(self.maintenance_text, tags=['br'], strip=True)
+        self.maintenance_text = bleach.clean(self.maintenance_text, tags=["br"], strip=True)
         super().save(*args, **kwargs)
 
     def __str__(self):
