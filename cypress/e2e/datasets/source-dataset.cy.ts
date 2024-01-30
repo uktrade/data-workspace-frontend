671581dcd07e0066d129d7c3461456cd75dcc145
Chris Hopkins <chris.hopkins@madetech.com>
2024-01-30 13:45:39 +0000

    add const for datasets

--- cypress/e2e/datasets/source-dataset.cy.ts
+++ cypress/e2e/datasets/source-dataset.cy.ts
@@ -1,6 +1,8 @@
+import { datacutWithLinks } from "../../fixtures/datasets";
+
 describe("Source dataset catalogue page", () => {
   beforeEach(() => {
-    cy.visit("/datasets/161d4b68-4b0d-4d96-80dc-d2f9867ff515");
+    cy.visit(`/datasets/${datacutWithLinks}`);
   });
 
   it("should have a title", () => {
---
+++ cypress/fixtures/datasets.ts
@@ -0,0 +1,1 @@
+export const datacutWithLinks = "161d4b68-4b0d-4d96-80dc-d2f9867ff515";
