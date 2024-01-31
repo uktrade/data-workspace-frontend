f5d2118ef0319adc4eefe16165777da63002259a
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2024-01-31 15:25:15 +0000

    bugfix/DT-1820-e2e-test-results (#2988)
    * upload test results
    
    * merge package dependancies
    
    * record videos
    
    * revert test fail

--- cypress.a11y.config.ts
+++ cypress.a11y.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
     specPattern: "cypress/a11y/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/a11y.ts",
     baseUrl: "http://dataworkspace.test:8000",
+    screenshotsFolder: "test-results/screenshots",
+    video:true,
+    videosFolder: "test-results/videos",
     viewportWidth: 1200,
     viewportHeight: 900,
     setupNodeEvents(on, config) {
--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://dataworkspace.test:8000",
+    screenshotsFolder: "test-results/screenshots",
+    video:true,
+    videosFolder: "test-results/videos",
     viewportWidth: 1200,
     viewportHeight: 900,
     setupNodeEvents(on, config) {
