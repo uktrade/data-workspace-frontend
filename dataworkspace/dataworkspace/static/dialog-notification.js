fe90c75261c2b8681bddf994184ebf2115305a49
Caitlin Barnard <caitlin@madetech.com>
2024-01-31 14:48:42 +0000

    Feedback link cookie

--- dataworkspace/dataworkspace/static/dialog-notification.js
+++ dataworkspace/dataworkspace/static/dialog-notification.js
@@ -1,8 +1,8 @@
-function setCookie(name, value, days) {
+function setCookie(name, value, expiry) {
   let expires = "";
-  if (days) {
+  if (expiry) {
     const date = new Date();
-    date.setTime(date.getTime() + (days*24*60*60*1000));
+    date.setTime(date.getTime() + expiry);
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie = `${name}=${value}${expires}; path=/`;
@@ -10,19 +10,23 @@ function setCookie(name, value, days) {
 
 function getCookie(name) {
   {
-    const regex = new RegExp(`(^| )${name}=([^;]+)`)
-    const match = document.cookie.match(regex)
+    const regex = new RegExp(`(^| )${name}=([^;]+)`);
+    const match = document.cookie.match(regex);
     if (match) {
-      return match[2]
+      return match[2];
     }
   }
 }
 
-function addButtonListener(elementId) {
+function addButtonListener(elementId, functionCall = showNotification) {
   var element = document.getElementById(elementId);
   if (element) {
-    element.addEventListener("click", showNotification);
+    element.addEventListener("click", functionCall);
   }
+}
+
+function showFeedbackBanner() {
+  document.getElementById("feedback-notification").style.display = "block";
 }
 
 function showNotification() {
@@ -30,11 +34,23 @@ function showNotification() {
   var oneWeek = 7 * 24 * 60 * 60 * 1000;
   var currentTime = new Date().getTime();
 
-  if (!lastShown || (currentTime - lastShown > oneWeek)) {
-    document.getElementById('feedbackNotification').style.display = 'block';
-    setCookie("notificationLastShown", currentTime, 7);
+  if (!lastShown || currentTime - lastShown > oneWeek) {
+    showFeedbackBanner();
+    setCookie("notificationLastShown", currentTime, oneWeek);
   }
+}
+
+function feedbackLinkCookie() {
+  var oneMinute = 60 * 1000;
+  var currentTime = new Date().getTime();
+
+  setCookie("feedbackLinkClicked", currentTime, oneMinute);
 }
 
 addButtonListener("data-grid-download");
 addButtonListener("data-grid-json-download");
+addButtonListener("feedback-link", feedbackLinkCookie);
+
+if (getCookie("feedbackLinkClicked") && document.referrer.includes("feedback")) {
+  showFeedbackBanner();
+}
