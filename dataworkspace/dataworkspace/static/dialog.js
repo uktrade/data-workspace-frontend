c481a2fd84eb00fd25780a693e1a58d1aa8161e8
Caitlin Barnard <caitlin@madetech.com>
2024-02-01 10:37:49 +0000

    Refactor dialog logic

--- dataworkspace/dataworkspace/static/dialog.js
+++ dataworkspace/dataworkspace/static/dialog.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = function() {
   const modal = document.getElementById("download-dialog");
   const closeButton = modal.querySelector('button:last-child');
-
+  
   closeButton.addEventListener("click", () => modal.close());
-
+  
   document.querySelectorAll(".modal-link").forEach((link) =>
     link.addEventListener("click", (e) => {
       e.preventDefault();
       modal.showModal();
     })
   );
-});
+}
