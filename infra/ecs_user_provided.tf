98d8067d5f605a59821088498129ab2a565548cd
Michal Charemza <michal@charemza.name>
2024-02-07 08:47:16 +0000

    refactor: move from template_file to templatefile
    This changes one use of the deprecated template_file provider to the now built
    in templatefile function in terraform.
    
    This is done because the template_file provider as no current version for the
    arm processor (e.g. modern macs), and so it should make it easier to apply the
    terraform.

--- infra/ecs_user_provided.tf
+++ infra/ecs_user_provided.tf
@@ -1,6 +1,20 @@
 resource "aws_ecs_task_definition" "user_provided" {
   family                = "${var.prefix}-user-provided"
-  container_definitions = "${data.template_file.user_provided_container_definitions.rendered}"
+  container_definitions    = templatefile(
+    "${path.module}/ecs_user_provided_container_definitions.json", {
+      container_name   = "${local.user_provided_container_name}"
+      container_cpu    = "${local.user_provided_container_cpu}"
+      container_memory = "${local.user_provided_container_memory}"
+      container_image  = "${aws_ecr_repository.user_provided.repository_url}"
+
+      log_group  = "${aws_cloudwatch_log_group.notebook.name}"
+      log_region = "${data.aws_region.aws_region.name}"
+
+      sentry_dsn = "${var.sentry_notebooks_dsn}"
+
+      metrics_container_image = "${aws_ecr_repository.metrics.repository_url}:master"
+    }
+  )
   execution_role_arn    = "${aws_iam_role.notebook_task_execution.arn}"
   network_mode          = "awsvpc"
   cpu                   = "${local.user_provided_container_cpu}"
@@ -21,24 +35,6 @@ data "external" "user_provided_metrics_current_tag" {
   query = {
     task_family = "${var.prefix}-user-provided"
     container_name = "metrics"
-  }
-}
-
-data "template_file" "user_provided_container_definitions" {
-  template = "${file("${path.module}/ecs_user_provided_container_definitions.json")}"
-
-  vars = {
-    container_name   = "${local.user_provided_container_name}"
-    container_cpu    = "${local.user_provided_container_cpu}"
-    container_memory = "${local.user_provided_container_memory}"
-    container_image  = "${aws_ecr_repository.user_provided.repository_url}"
-
-    log_group  = "${aws_cloudwatch_log_group.notebook.name}"
-    log_region = "${data.aws_region.aws_region.name}"
-
-    sentry_dsn = "${var.sentry_notebooks_dsn}"
-
-    metrics_container_image = "${aws_ecr_repository.metrics.repository_url}:master"
   }
 }
 
