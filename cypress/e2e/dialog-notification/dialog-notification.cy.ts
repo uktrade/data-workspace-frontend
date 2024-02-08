a6186106980b911f4e89406b22880759abc46cbe
Caitlin Barnard <caitlin@madetech.com>
2024-02-08 10:49:44 +0000

    Update tests to match fixtures

--- cypress/e2e/dialog-notification/dialog-notification.cy.ts
+++ cypress/e2e/dialog-notification/dialog-notification.cy.ts
@@ -8,13 +8,13 @@ describe("Dialog notification", () => {
         body: '',
         headers: {
           'Content-Type': 'text/csv',
-          'Content-Disposition': 'attachment; filename=source_link1.csv'
+          'Content-Disposition': 'attachment; filename=source_link_1.csv'
         }
       }).as("getDownload");
 
       cy.visit(`/datasets/${datacutWithLinks}`);
-      cy.findByRole("link", { name: "source_link1" }).click();
-      cy.findByRole("link", { name: "Download data as csv" }).click();
+      cy.findByRole("link", { name: "Source link 1" }).click();
+      cy.findByRole("link", { name: "Download data as CSV" }).click();
       cy.wait("@getDownload")
     }
 
@@ -22,7 +22,9 @@ describe("Dialog notification", () => {
       visitPageAndDownload();
 
       cy.findByRole("dialog").should('be.visible');
-      cy.findByText("Important").should("be.visible");
+      cy.findByRole("dialog").within(() => {
+        cy.findByText("Important").should("be.visible");
+      });
     });
 
     it("should create a cookie when one does not exist", () => {
@@ -36,7 +38,9 @@ describe("Dialog notification", () => {
       visitPageAndDownload();
 
       cy.findByRole("dialog").should('exist');
-      cy.findByText("Important").should("not.be.visible");
+      cy.findByRole("dialog").within(() => {
+        cy.findByText("Important").should("not.be.visible");
+      });
     });
 
     it("should have a link to the feedback form with a query parameter", () => {
@@ -59,14 +63,16 @@ describe("Dialog notification", () => {
         cy.findByLabelText("Analyse data").should("be.checked");
       });
 
-      it.only("should show dialog modal and banner notification when user clicks back", () => {
+      it("should show dialog modal and banner notification when user clicks back", () => {
         visitPageAndDownload();
 
         cy.findByRole("link", { name: "feedback form." }).click();
         cy.findByRole("link", { name: "Back" }).click();
 
         cy.findByRole("dialog").should('be.visible');
-        cy.findByText("Important").should("be.visible");
+        cy.findByRole("dialog").within(() => {
+          cy.findByText("Important").should("be.visible");
+        });
       });
     });
   });
