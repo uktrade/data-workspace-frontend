598dda2880a112187d3940aa8edbfce6d810b0a0
Ian Leggett <contactleggett@gmail.com>
2024-02-09 10:25:39 +0000

    Revert "Revert "Add feedback banner for data download with cookie""

---
+++ dataworkspace/dataworkspace/static/dialog-notification.js
@@ -0,0 +1,50 @@
+function setCookie(name, value, expiry) {
+  let expires = "";
+  if (expiry) {
+    const date = new Date();
+    date.setTime(date.getTime() + expiry);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = `${name}=${value}${expires}; path=/`;
+}
+
+function getCookie(name) {
+  {
+    const regex = new RegExp(`(^| )${name}=([^;]+)`);
+    const match = document.cookie.match(regex);
+    if (match) {
+      return match[2];
+    }
+  }
+}
+
+function addButtonListener(elementId, functionCall = showNotification) {
+  var element = document.getElementById(elementId);
+  if (element) {
+    element.addEventListener("click", functionCall);
+  }
+}
+
+function showFeedbackBanner() {
+  document.getElementById("feedback-notification").style.display = "block";
+}
+
+function showNotification() {
+  var lastShown = getCookie("notificationLastShown");
+  var oneWeek = 7 * 24 * 60 * 60 * 1000;
+  var currentTime = new Date().getTime();
+
+  if (!lastShown || currentTime - lastShown > oneWeek) {
+    showFeedbackBanner();
+    setCookie("notificationLastShown", currentTime, oneWeek);
+  }
+}
+
+addButtonListener("data-link-download");
+addButtonListener("data-grid-download");
+addButtonListener("data-grid-json-download");
+
+if (document.referrer.includes("feedback")) {
+  showFeedbackBanner();
+  document.getElementById("download-dialog").showModal();
+}
--- dataworkspace/dataworkspace/static/dialog.js
+++ dataworkspace/dataworkspace/static/dialog.js
@@ -1,14 +1,15 @@
-document.addEventListener("DOMContentLoaded", () =>
-  document.querySelectorAll(".modal-link").forEach((link) =>
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const modal = e.target.nextElementSibling;
-      modal.showModal();
-      const closeButton =
-        modal.getElementsByTagName("button")[
-          modal.getElementsByTagName("button").length - 1
-        ];
-      closeButton.addEventListener("click", () => modal.close());
-    })
-  )
-);
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("download-dialog");
+  if (modal) {
+    const closeButton = modal.querySelector('button:last-child');
+    
+    closeButton.addEventListener("click", () => modal.close());
+    
+    document.querySelectorAll(".modal-link").forEach((link) =>
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.showModal();
+      })
+    );
+  }
+});
