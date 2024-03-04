089073bb1971f44eaa95cf244c6bf171fff31ba7
Chris Hopkins <chris.hopkins@madetech.com>
2024-03-04 11:31:10 +0000

    add a a11y test

---
+++ cypress/a11y/collections.cy.ts
@@ -0,0 +1,10 @@
+describe("Collections page", () => {
+  beforeEach(() => {
+    cy.visit(`/collections`);
+    cy.injectAxe();
+  });
+
+  it("Check entire page for a11y issues", () => {
+    cy.checkA11y();
+  });
+});
