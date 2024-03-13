a895aaeca4f7bc5ec323b2d4d7463d5eb5888e46
Chris Hopkins <chris.hopkins@madetech.com>
2024-03-13 09:15:51 +0000

    save circle jobs

--- dataworkspace/dataworkspace/static/js/jest.config.js
+++ dataworkspace/dataworkspace/static/js/jest.config.js
@@ -5,5 +5,13 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest'
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  collectCoverageFrom: ['**/react/**/*.{ts,tsx}'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      { outputDirectory: 'test-results', outputName: 'results.xml' }
+    ]
+  ]
 };
