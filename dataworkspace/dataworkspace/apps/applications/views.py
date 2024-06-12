c219e2ad292da678e01edd7e977657fa7f847e8f
Caitlin Barnard <caitlin@madetech.com>
2024-06-12 09:51:34 +0100

    Set success message to appear when referer is self certify page

--- dataworkspace/dataworkspace/apps/applications/views.py
+++ dataworkspace/dataworkspace/apps/applications/views.py
@@ -204,12 +204,11 @@ def tools_html_view(request):
 
 def tools_html_GET(request):
     tools = get_grouped_tools(request)
-
+    is_self_certify = "self-certify" in request.META.get("HTTP_REFERER")
     return render(
         request,
         "applications/tools.html",
-        {"tools": tools,
-        "is_self_certify": True},
+        {"tools": tools, "is_self_certify": is_self_certify},
     )
 
 
