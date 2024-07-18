d20eba5a01db381107c84ed78d811bca8f6a1391
Ian leggett <>
2024-07-18 15:09:00 +0100

    pass dataset name to template and update test

--- dataworkspace/dataworkspace/apps/datasets/add_table/views.py
+++ dataworkspace/dataworkspace/apps/datasets/add_table/views.py
@@ -1,11 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView
 from django.urls import reverse
+from dataworkspace.apps.datasets import models
+from dataworkspace.apps.datasets.utils import find_dataset
 
 
-class AddTableView(TemplateView):
+class AddTableView(DetailView):
     template_name = "datasets/add_table/about_this_service.html"
 
+    def get_object(self, queryset=None):
+        return find_dataset(self.kwargs["pk"], self.request.user)
+
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["backlink"] = reverse("datasets:dataset_detail", args={kwargs["pk"]})
-        return context
+        ctx = super().get_context_data(**kwargs)
+        ctx["model"] = self.object
+        ctx["backlink"] = reverse("datasets:dataset_detail", args={self.kwargs["pk"]})
+
+        return ctx
--- dataworkspace/dataworkspace/tests/datasets/add_table/test_views.py
+++ dataworkspace/dataworkspace/tests/datasets/add_table/test_views.py
@@ -8,21 +8,28 @@ from dataworkspace.tests.common import get_http_sso_data
 
 
 @pytest.mark.django_db
-def test_about_service():
+def test_about_service_page():
     user = factories.UserFactory.create(is_superuser=False)
     client = Client(**get_http_sso_data(user))
     dataset = factories.MasterDataSetFactory.create(
         published=True, user_access_type=UserAccessType.REQUIRES_AUTHORIZATION
     )
-    print("dataset", dataset.id)
+
     response = client.get(
         reverse("datasets:add_table:add-table", kwargs={"pk": dataset.id}),
     )
     soup = BeautifulSoup(response.content.decode(response.charset))
     header_one = soup.find("h1")
     header_two = soup.find("h2")
+    paragraph = soup.find("p")
     header_one_text = header_one.contents
     header_two_text = header_two.contents
+    paragraph_text = paragraph.contents
+    print("paragraph", paragraph_text)
     assert response.status_code == 200
     assert "About this service" in header_one_text
     assert "When you should not use this service" in header_two_text
+    assert (
+        f"Use this service to turn a CSV file into a data table. The new table will be added to {dataset.name}."
+        in paragraph_text
+    )
