38f8bfe6563d656276a7feb1e3eceb7273f18512
Sophie Glinton <sophieglinton@dit000153.home>
2024-07-31 09:46:45 +0100

    Integration test added for team database access through django permissions model

--- test/test_application_1.py
+++ test/test_application_1.py
@@ -27,6 +27,9 @@ from test.utility_functions import (
     give_user_visualisation_developer_perms,
     sync_query_logs,
     until_succeeds,
+    until_non_202,
+    ensure_arango_team_created,
+    add_user_to_arango_team,
 )
 
 from aiohttp import web
@@ -919,3 +922,63 @@ class TestApplication(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(
             payload["authorised_hosts"], ["mlflow--data-science--internal.dataworkspace.test:8000"]
         )
+
+    async def test_arango_team_database_permissions(self):
+        session, cleanup_session = client_session()
+        self.addAsyncCleanup(cleanup_session)
+
+        cleanup_application = await create_application()
+        self.addAsyncCleanup(cleanup_application)
+
+        is_logged_in = True
+        codes = iter(["some-code"])
+        tokens = iter(["token-1"])
+        auth_to_me = {
+            "Bearer token-1": {
+                "email": "test@test.com",
+                "contact_email": "test@test.com",
+                "related_emails": [],
+                "first_name": "Peter",
+                "last_name": "Piper",
+                "user_id": "7f93c2c7-bc32-43f3-87dc-40d0b8fb2cd2",
+            }
+        }
+        sso_cleanup, _ = await create_sso(is_logged_in, codes, tokens, auth_to_me)
+        self.addAsyncCleanup(sso_cleanup)
+
+        await until_succeeds("http://dataworkspace.test:8000/healthcheck")
+
+        stdout, stderr, code = await give_user_app_perms()
+        self.assertEqual(stdout, b"")
+        self.assertEqual(stderr, b"")
+        self.assertEqual(code, 0)
+
+        # Create arango team - 'Test1' database with team member access in ArangoDB
+        _, _, code = await ensure_arango_team_created(team_name="Test1")
+        self.assertEqual(code, 0)
+        _, _, code = await add_user_to_arango_team(
+            user_sso_id="7f93c2c7-bc32-43f3-87dc-40d0b8fb2cd2", team_name="Test1"
+        )
+        self.assertEqual(code, 0)
+
+        # Create arango team - 'Test2' database with no access in ArangoDB
+        _, _, code = await ensure_arango_team_created(team_name="Test2")
+        self.assertEqual(code, 0)
+
+        async with session.request(
+            "GET", "http://testdbapplication-23b40dd9.dataworkspace.test:8000/"
+        ) as response:
+            application_content_1 = await response.text()
+
+        self.assertIn("Tool with DB access is loading...", application_content_1)
+
+        await until_non_202(session, "http://testdbapplication-23b40dd9.dataworkspace.test:8000/")
+
+        async with session.request(
+            "GET", "http://testdbapplication-23b40dd9.dataworkspace.test:8000/arango"
+        ) as response:
+            status = response.status
+            content = await response.text()
+
+        self.assertEqual(status, 200)
+        self.assertEqual('{"data": {"team_test1": "rw", "team_test2": "none"}}', content)
