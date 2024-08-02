ed76d0a628d953d04113470335fa695b635e328d
Caitlin Barnard <caitlin@madetech.com>
2024-08-02 11:57:00 +0100

    Add test to check when multiple schemas that there are no duplicates

--- dataworkspace/dataworkspace/tests/datasets/add_table/test_views.py
+++ dataworkspace/dataworkspace/tests/datasets/add_table/test_views.py
@@ -101,7 +101,7 @@ class TestAddTable(TestCase):
         )
         assert self.source.schema and self.source2.schema in radio_names
 
-    def test_table_schema_page_when_multiple_tables_have_same_schema(self):
+    def test_table_schema_page_when_all_tables_have_same_schema(self):
         self.source2 = factories.SourceTableFactory.create(
             dataset=self.dataset, schema="test", table="table2"
         )
@@ -115,6 +115,7 @@ class TestAddTable(TestCase):
         header_two = soup.find("h2")
         paragraph = soup.find("p")
         title = soup.find("title")
+        radios = soup.find_all("input", type="radio")
         header_one_text = header_one.contents
         header_two_text = header_two.contents
         paragraph_text = paragraph.contents
@@ -128,3 +129,39 @@ class TestAddTable(TestCase):
             "This is the schema used by other tables in this catalogue item. Schemas are used to categorise data sources. Schemas are often named after the data provider e.g. HMRC."
             in paragraph_text
         )
+
+    def test_table_schema_page_when_some_tables_have_same_schema(self):
+        self.source2 = factories.SourceTableFactory.create(
+            dataset=self.dataset, schema="dbt", table="table2"
+        )
+        self.source2 = factories.SourceTableFactory.create(
+            dataset=self.dataset, schema="dbt", table="table3"
+        )
+
+        response = self.client.get(
+            reverse("datasets:add_table:table-schema", kwargs={"pk": self.dataset.id}),
+        )
+
+        soup = BeautifulSoup(response.content.decode(response.charset))
+        header_one = soup.find("h1")
+        header_two = soup.find("h2")
+        paragraph = soup.find("p")
+        title = soup.find("title")
+        radios = soup.find_all("input", type="radio")
+        header_one_text = header_one.contents
+        header_two_text = header_two.contents
+        paragraph_text = paragraph.contents
+        title_text = title.contents[0]
+        radio_names = (radio.get("value") for radio in radios)
+
+        assert response.status_code == 200
+        assert f"Add Table - {self.dataset.name} - Data Workspace" in title_text
+        assert "Your table's schema" in header_one_text
+        assert "Select an existing schema from this catalogue page" in header_two_text
+        assert (
+            "This is the schema used by other tables in this catalogue item. Schemas are used to categorise data sources. Schemas are often named after the data provider e.g. HMRC."
+            in paragraph_text
+        )
+
+        schemas = list(radio_names)
+        assert len(schemas) == 2
