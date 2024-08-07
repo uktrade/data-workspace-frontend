50fb0cb9be8d9dc45767407dcb6e1d2b93665509
Sophie Glinton <sophieglinton@DIT000153.local>
2024-08-07 16:21:21 +0100

    ArangoUser model and unit testing of utils.

--- dataworkspace/dataworkspace/tests/core/factories.py
+++ dataworkspace/dataworkspace/tests/core/factories.py
@@ -1,12 +1,22 @@
 import uuid
 import factory
-from dataworkspace.apps.core.models import Team
+from dataworkspace.tests.factories import UserFactory
+from dataworkspace.apps.core.models import Team, TeamMembership
 
 
 class TeamFactory(factory.django.DjangoModelFactory):
     id = factory.LazyAttribute(lambda _: uuid.uuid4())
     name = factory.fuzzy.FuzzyText(length=20)
     schema_name = factory.fuzzy.FuzzyText(length=20)
+    platform = factory.fuzzy.FuzzyChoice(["postgres", "postgres-and-arango"])
 
     class Meta:
         model = Team
+
+
+class TeamMembershipFactory(factory.django.DjangoModelFactory):
+    team = factory.SubFactory(TeamFactory)
+    user = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = TeamMembership
