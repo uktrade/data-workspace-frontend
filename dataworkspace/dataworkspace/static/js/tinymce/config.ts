5262be339a8047d6ff03bd09b47b102db005f89c
Ian leggett <>
2024-08-09 15:23:51 +0100

    wip

--- dataworkspace/dataworkspace/static/js/tinymce/config.ts
+++ dataworkspace/dataworkspace/static/js/tinymce/config.ts
@@ -1,29 +1,33 @@
-import type { RawEditorOptions } from "tinymce";
-import tinymce from "tinymce";
+import type { RawEditorOptions } from 'tinymce';
+import tinymce from 'tinymce';
 
-type TinyMceOptions = Partial<RawEditorOptions>
+type TinyMceOptions = Partial<RawEditorOptions>;
 
 const tinymce_default_config: TinyMceOptions = {
-  height: "320px",
-  width: "900px",
+  height: '320px',
+  width: '900px',
   menubar: false,
   custom_undo_redo_levels: 10,
-  license_key: "gpl",
-  plugins: "advlist autolink lists link searchreplace visualblocks code codesample fullscreen insertdatetime media table code wordcount",
+  license_key: 'gpl',
+  plugins:
+    'advlist autolink lists link searchreplace visualblocks code codesample fullscreen insertdatetime media table code wordcount',
   toolbar:
-    "undo redo | blocks | bold italic | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | numlist bullist checklist | link codesample code",
+    'undo redo | blocks | bold italic | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | numlist bullist checklist | link codesample code',
 };
 
 const tinymce_link_config: TinyMceOptions = {
   ...tinymce_default_config,
-  plugins: ["link"],
-  toolbar: "link",
+  plugins: ['link'],
+  toolbar: 'link'
 };
 
-const initTinymce = (selector: string, config: TinyMceOptions = tinymce_default_config) => {
+const initTinymce = (
+  selector: string,
+  config: TinyMceOptions = tinymce_default_config
+) => {
   tinymce.init({
     selector: `textarea${selector}`,
-    ...config,
+    ...config
   });
 };
 
