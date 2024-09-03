06862cadf97543a7533f63b79bd4fcef1aefe7cb
Caitlin Barnard <caitlin@madetech.com>
2024-09-03 15:55:28 +0100

    Flake8 2

--- dataworkspace/dataworkspace/apps/datasets/add_table/views.py
+++ dataworkspace/dataworkspace/apps/datasets/add_table/views.py
@@ -30,6 +30,7 @@ from dataworkspace.apps.your_files.utils import get_s3_csv_file_info
 from dataworkspace.apps.your_files.views import (
     RequiredParameterGetRequestMixin,
 )
+from dataworkspace.dataworkspace.apps.core.models import Database
 
 
 class AddTableView(DetailView):
