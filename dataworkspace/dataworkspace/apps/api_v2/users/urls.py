495e4b2eaf81e02b5e6fee7f13df1075a33ded58
jamesprobinson <james.robinson@businessandtrade.gov.uk>
2024-11-06 13:13:32 +0000

    fix/remove comment

--- dataworkspace/dataworkspace/apps/api_v2/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from dataworkspace.apps.api_v2.users import PendingAuthorizedUsersViewSet
-
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r"summary_pending_users", PendingAuthorizedUsersViewSet)
-urlpatterns = router.urls
