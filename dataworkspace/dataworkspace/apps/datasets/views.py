1e552ebdcf0ba696cd55954ad1f2eda6c5c69598
jamesprobinson <james.robinson@businessandtrade.gov.uk>
2024-11-14 13:00:33 +0000

    fix/remove data user permission object

--- dataworkspace/dataworkspace/apps/datasets/views.py
+++ dataworkspace/dataworkspace/apps/datasets/views.py
@@ -1804,24 +1804,28 @@ class DatasetEditPermissionsSummaryView(EditBaseView, TemplateView):
             iam = get_user_model().objects.get(id=self.obj.information_asset_manager_id).email
             iao = get_user_model().objects.get(id=self.obj.information_asset_owner_id).email
             context["authorised_users"] = json.dumps(
-                [
-                    {
-                        "data_catalogue_editor": u.email in data_catalogue_editors,
-                        "email": u.email,
-                        "first_name": u.first_name,
-                        "iam": u.email == iam,
-                        "iao": u.email == iao,
-                        "id": u.id,
-                        "last_name": u.last_name,
-                        "remove_user_url": reverse(
-                            "datasets:remove_authorized_user",
-                            args=[self.obj.id, self.summary.id, u.id],
-                        ),
-                    }
-                    for u in get_user_model().objects.filter(
-                        id__in=json.loads(self.summary.users) if self.summary.users else []
-                    )
-                ]
+                sorted(
+                    [
+                        {
+                            "data_catalogue_editor": u.email in data_catalogue_editors,
+                            "email": u.email,
+                            "first_name": u.first_name,
+                            "iam": u.email == iam,
+                            "iao": u.email == iao,
+                            "id": u.id,
+                            "last_name": u.last_name,
+                            "remove_user_url": reverse(
+                                "datasets:remove_authorized_user",
+                                args=[self.obj.id, self.summary.id, u.id],
+                            ),
+                        }
+                        for u in get_user_model().objects.filter(
+                            id__in=json.loads(self.summary.users) if self.summary.users else []
+                        )
+                    ],
+                    key=lambda x: x["iam"] or x["iao"],
+                    reverse=True,
+                )
             )
 
             requests = AccessRequest.objects.filter(
@@ -2069,6 +2073,17 @@ class DatasetRemoveAuthorisedUserView(EditBaseView, View):
         if user.id in users:
             summary.users = json.dumps([user_id for user_id in users if user_id != user.id])
             summary.save()
+        users = set(get_user_model().objects.exclude(id__in=[user.id]))
+        if isinstance(self.obj, DataSet):
+            process_dataset_authorized_users_change(
+                users, request.user, self.obj, False, False, True
+            )
+            messages.success(request, "Dataset permissions updated")
+        else:
+            process_visualisation_catalogue_item_authorized_users_change(
+                users, request.user, self.obj, False, False
+            )
+            messages.success(request, "Visualisation permissions updated")
         if waffle.flag_is_active(self.request, settings.ALLOW_REQUEST_ACCESS_TO_DATA_FLOW):
             name_dataset = find_dataset(self.obj.pk, request.user).name
             url_dataset = request.build_absolute_uri(
@@ -2082,7 +2097,6 @@ class DatasetRemoveAuthorisedUserView(EditBaseView, View):
                     "dataset_url": url_dataset,
                 },
             )
-
         return HttpResponseRedirect(
             reverse(
                 "datasets:edit_permissions_summary",
