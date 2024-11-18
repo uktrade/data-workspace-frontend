0dba5d655861c49881b7430fa1a1a56d04445cc2
James Robinson <james.robinson@businessandtrade.gov.uk>
2024-11-18 21:32:28 +0000

    add styled

--- dataworkspace/dataworkspace/static/js/react/features/home-page/components/ManageData/index.tsx
+++ dataworkspace/dataworkspace/static/js/react/features/home-page/components/ManageData/index.tsx
@@ -9,7 +9,7 @@ import { Tile } from '../../../../components';
 
 export type ManagedDataProps = {
   count: string;
-  managed_data_url: string | null;
+  managed_data_url: string;
 };
 
 const StyledParagraph = styled('p')`
@@ -23,7 +23,7 @@ const ManagedData: React.FC<Record<'managed_data_stats', ManagedDataProps>> = ({
     {managed_data_stats && (
       <Tile
         as="article"
-        title={`You're the owner or manager of ${managed_data_stats.count} datasets`}
+        title={`You're the owner or manager of ${managed_data_stats.count} dataset${Number(managed_data_stats.count) > 1 ? 's' : ''}`}
       >
         <div>
           <Link href={`${managed_data_stats.managed_data_url}`}>
@@ -35,8 +35,8 @@ const ManagedData: React.FC<Record<'managed_data_stats', ManagedDataProps>> = ({
           Keeping your data up-to-date helps improve the quality of our data
           catalogue.{' '}
           <Link href="https://data-services-help.trade.gov.uk/data-workspace/add-share-and-manage-data/manage-data/">
-            Learn how to maintain and managed data you're responsible for on
-            Data Workspace
+            Learn how to maintain and manage data you're responsible for on Data
+            Workspace
           </Link>
         </StyledParagraph>
       </Tile>
