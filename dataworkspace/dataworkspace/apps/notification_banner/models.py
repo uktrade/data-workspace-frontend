5c7962056700d21e3216291afe8822eb18900a31
Ledia Luli <ledia.luli@digital.trade.gov.uk>
2025-01-24 16:19:37 +0000

    styling

--- dataworkspace/dataworkspace/apps/notification_banner/models.py
+++ dataworkspace/dataworkspace/apps/notification_banner/models.py
@@ -13,17 +13,14 @@ class NotificationBanner(models.Model):
 
     def save(self, *args, **kwargs):
         self.content = bleach.clean(self.content, tags=["br"], strip=True)
-        # Create only one Abc instance
+        # Create only one NotificationBanner instance
         if not self.pk and NotificationBanner.objects.exists():
             # This below line will render error by breaking page, you will see
             raise ValidationError(
                 "There can be only one NotificationBanner you can not add another"
             )
-            # OR you can ever return None from here,
-            # this will not save any data only you can update existing once
             return None
         return super(NotificationBanner, self).save(*args, **kwargs)
-        # super().save(*args, **kwargs)
 
     def __str__(self):
         return "Banner Settings"
