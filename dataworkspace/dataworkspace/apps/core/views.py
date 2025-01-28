2e6731c54352b839edde55f33577a22efd4347a9
Ledia Luli <ledia.luli@digital.trade.gov.uk>
2025-01-28 11:02:27 +0000

    codescanning

--- dataworkspace/dataworkspace/apps/core/views.py
+++ dataworkspace/dataworkspace/apps/core/views.py
@@ -531,5 +531,6 @@ class SetNotificationCookie(View):
             expires=date_expiry.strftime("%a, %d-%b-%Y %H:%M:%S GMT"),
             httponly=True,
             samesite="Lax",
+            secure=True',
         )
         return response
