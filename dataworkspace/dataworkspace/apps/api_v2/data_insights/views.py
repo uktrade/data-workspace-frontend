111bb99bb46f8850b0778d859c6f59058e6938af
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-01-31 10:29:05 +0000

    fix/api tests

--- dataworkspace/dataworkspace/apps/api_v2/data_insights/views.py
+++ dataworkspace/dataworkspace/apps/api_v2/data_insights/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
 from dataworkspace.apps.api_v2.data_insights.serializers import OwnerInsightsSerializer
 
@@ -13,3 +15,5 @@ class OwnerInsightsViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = OwnerInsightsSerializer
     pagination_class = PageNumberPagination
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
