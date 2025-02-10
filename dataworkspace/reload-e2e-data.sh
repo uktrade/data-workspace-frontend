8a31fd2ccea09612870c4a18d909d49a23319dce
Ian leggett <>
2025-02-10 14:58:39 +0000

    chore: remove feature flag

--- dataworkspace/reload-e2e-data.sh
+++ dataworkspace/reload-e2e-data.sh
@@ -2,8 +2,6 @@
 
 django-admin flush --noinput
 echo "Cleared existing django data from DB"
-django-admin waffle_flag HOME_PAGE_FLAG --everyone --create
-django-admin waffle_flag ACCESSIBLE_AUTOCOMPLETE_FLAG --everyone --create
 django-admin waffle_flag SUGGESTED_SEARCHES_FLAG --everyone --create
 echo "Feature flags have been set"
 django-admin loaddata --ignorenonexistent --verbosity=3 dataworkspace/e2e_fixtures.json
