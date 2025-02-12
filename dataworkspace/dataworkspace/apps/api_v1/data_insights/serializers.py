9f3172f5e149412cb56d3996aee49eb83f10c703
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-02-12 09:21:08 +0000

    fix/wrong attr

--- dataworkspace/dataworkspace/apps/api_v1/data_insights/serializers.py
+++ dataworkspace/dataworkspace/apps/api_v1/data_insights/serializers.py
@@ -66,7 +66,7 @@ class OwnerInsightsSerializer(serializers.ModelSerializer):
         service_ds = DataDictionaryService()
         source_table_response = []
         for source_table in source_tables:
-            if not table_exists(source_table.database, source_table.schema, source_table.name):
+            if not table_exists(source_table.database.memorable_name, source_table.schema, source_table.name):
                 continue
             source_table_response.append(
                 {
