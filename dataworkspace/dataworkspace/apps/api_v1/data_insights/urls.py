71b7901f207d3e746cdbdc95029b70fcd0dd1af7
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-02-13 13:38:04 +0000

    fix/url path owner insights

--- dataworkspace/dataworkspace/apps/api_v1/data_insights/urls.py
+++ dataworkspace/dataworkspace/apps/api_v1/data_insights/urls.py
@@ -1,7 +1,11 @@
-from rest_framework.routers import DefaultRouter
-
 from dataworkspace.apps.api_v1.data_insights.views import OwnerInsightsViewSet
 
-router = DefaultRouter()
-router.register(r"owners", OwnerInsightsViewSet)
-urlpatterns = router.urls
+from django.urls import path
+
+urlpatterns = [
+    path(
+        "owners/insights/",
+        OwnerInsightsViewSet.as_view({"get": "get_owner_insights"}),
+        name="owners-insights",
+    ),
+]
