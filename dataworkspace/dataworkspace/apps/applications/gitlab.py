f3143d48488015edf834f49b559b43c1169c92ce
Ledia Luli <ledia.luli@digital.trade.gov.uk>
2025-02-18 15:03:26 +0000

    adding function check for dataworkspace team member

--- dataworkspace/dataworkspace/apps/applications/gitlab.py
+++ dataworkspace/dataworkspace/apps/applications/gitlab.py
@@ -121,6 +121,13 @@ def gitlab_has_developer_access(user, gitlab_project_id):
     return has_access
 
 
+def is_dataworkspace_team_member(user, gitlab_project_id):
+    if user.is_superuser and gitlab_has_developer_access(user, gitlab_project_id):
+        return True
+    else:
+        return False
+
+
 def _ensure_user_has_manage_unpublish_perm(user):
     # Update the django permission controlling whether the user can preview unpublished visualisation catalogue pages.
     perm_codename = dataset_type_to_manage_unpublished_permission_codename(
--- dataworkspace/dataworkspace/tests/applications/test_gitlab.py
+++ dataworkspace/dataworkspace/tests/applications/test_gitlab.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from django.test import override_settings
 
-from dataworkspace.apps.applications.gitlab import gitlab_has_developer_access
+from dataworkspace.apps.applications.gitlab import gitlab_has_developer_access, is_dataworkspace_team_member
 from dataworkspace.apps.datasets.constants import DataSetType
 from dataworkspace.apps.datasets.utils import (
     dataset_type_to_manage_unpublished_permission_codename,
@@ -48,4 +48,77 @@ class TestGitlabHasDeveloperAccess:
         # https://docs.djangoproject.com/en/3.0/topics/auth/default/#permission-caching
         user = get_object_or_404(get_user_model(), pk=user.id)
         assert has_access is True
+        assert user.has_perm(perm_codename) is True
+
+
+class TestIsDataWorkspaceTeamMember:
+    @override_settings(
+        CACHES={"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
+    )
+    @pytest.mark.django_db
+    def test_dataworkspace_team_member_has_permission(self):
+        user = factories.UserFactory.create(
+            username="visualisation.creator@test.com",
+            is_staff=True,
+            is_superuser=True,
+        )
+        visualisation = factories.VisualisationCatalogueItemFactory.create(
+            published=False, visualisation_template__gitlab_project_id=1
+        )
+        perm_codename = dataset_type_to_manage_unpublished_permission_codename(
+            DataSetType.VISUALISATION
+        )
+        assert user.has_perm(perm_codename) is True
+
+        with requests_mock.Mocker() as rmock:
+            rmock.get(
+                f"http://127.0.0.1:8007/api/v4/users?extern_uid={user.profile.sso_id}&provider=oauth2_generic",
+                json=[{"id": 1}],
+            )
+            rmock.get(
+                "http://127.0.0.1:8007/api/v4/projects/1/members/all?user_ids=1",
+                json=[{"id": 1, "access_level": 50}],
+            )
+            has_access_as_dataworkspace_team_member = is_dataworkspace_team_member(
+                user, visualisation.visualisation_template.gitlab_project_id
+            )
+
+        # Permissions are cached on the instance so we need to re-fetch it entirely - refresh_from_db insufficient.
+        # https://docs.djangoproject.com/en/3.0/topics/auth/default/#permission-caching
+        user = get_object_or_404(get_user_model(), pk=user.id)
+        assert has_access_as_dataworkspace_team_member is True
+        assert user.has_perm(perm_codename) is True
+
+    @pytest.mark.django_db
+    def test_non_dataworkspace_team_member_does_not_have_permission(self):
+        user = factories.UserFactory.create(
+            username="visualisation.creator@test.com",
+            is_staff=True,
+            is_superuser=False,
+        )
+        visualisation = factories.VisualisationCatalogueItemFactory.create(
+            published=False, visualisation_template__gitlab_project_id=1
+        )
+        perm_codename = dataset_type_to_manage_unpublished_permission_codename(
+            DataSetType.VISUALISATION
+        )
+        assert user.has_perm(perm_codename) is False
+
+        with requests_mock.Mocker() as rmock:
+            rmock.get(
+                f"http://127.0.0.1:8007/api/v4/users?extern_uid={user.profile.sso_id}&provider=oauth2_generic",
+                json=[{"id": 1}],
+            )
+            rmock.get(
+                "http://127.0.0.1:8007/api/v4/projects/1/members/all?user_ids=1",
+                json=[{"id": 1, "access_level": 50}],
+            )
+            has_access_as_dataworkspace_team_member = is_dataworkspace_team_member(
+                user, visualisation.visualisation_template.gitlab_project_id
+            )
+
+        # Permissions are cached on the instance so we need to re-fetch it entirely - refresh_from_db insufficient.
+        # https://docs.djangoproject.com/en/3.0/topics/auth/default/#permission-caching
+        user = get_object_or_404(get_user_model(), pk=user.id)
+        assert has_access_as_dataworkspace_team_member is False
         assert user.has_perm(perm_codename) is True
