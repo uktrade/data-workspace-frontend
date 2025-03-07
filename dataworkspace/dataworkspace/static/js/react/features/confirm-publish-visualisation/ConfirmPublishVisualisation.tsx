702b378a3d35f51cfcd79f98eb89bface0e12272
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-03-07 11:40:53 +0000

    initial setup of feature

---
+++ dataworkspace/dataworkspace/static/js/react/features/confirm-publish-visualisation/ConfirmPublishVisualisation.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import { Button } from '@govuk-react/button';
+
+import ConfirmDialog from '../../components/ConfirmDialog/';
+
+type PublishUrl = {
+  publishUrl: string;
+};
+
+const ConfirmPublishVisualisation = ({
+  data
+}: {
+  data: PublishUrl;
+}): React.ReactNode => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  return (
+    <>
+      <Button
+        buttonColour="#f3f2f1"
+        buttonTextColour="#0b0c0c"
+        onClick={() => openModal()}
+      >
+        Publish Visualisation
+      </Button>
+      {isOpen && (
+        <ConfirmDialog
+          actionUrl={data.publishUrl}
+          bodyText={
+            'You‘re responsible for the information security and data protection of the data this visualisation uses. All data must be published to the Data Workspace catalogue. Storing and using data from Gitlab is not permitted.'
+          }
+          title={'Final review before publishing'}
+          open={isOpen}
+          onClose={closeModal}
+          buttonTextAccept={'Publish to catalogue'}
+          buttonTextCancel={'Close'}
+          warning={true}
+        ></ConfirmDialog>
+      )}
+    </>
+  );
+};
+
+export default ConfirmPublishVisualisation;
