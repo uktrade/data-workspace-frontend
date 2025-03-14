4fbfb5148dcfbb0fa97f17a4a70950d941ab5ccf
Ledia Luli <ledia.luli@digital.trade.gov.uk>
2025-03-14 15:03:13 +0000

    formatting

--- dataworkspace/dataworkspace/apps/datasets/urls.py
+++ dataworkspace/dataworkspace/apps/datasets/urls.py
@@ -6,7 +6,6 @@ from dataworkspace.apps.datasets.data_dictionary import views as data_dictionary
 from dataworkspace.apps.datasets.search import suggested_searches
 from dataworkspace.apps.datasets.subscriptions import views as subscription_views
 from dataworkspace.apps.request_access.views import DatasetAccessRequest
-from dataworkspace.apps.datasets.requesting_data.views import RequestingDataWizardView
 
 urlpatterns = [
     path("", login_required(views.find_datasets), name="find_datasets"),
@@ -305,7 +304,6 @@ urlpatterns = [
             namespace="add_table",
         ),
     ),
-
     path(
         "<uuid:pk>/review-access/<int:user_id>",
         login_required(views.DataSetReviewAccess.as_view()),
