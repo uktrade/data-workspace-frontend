085354fc5a44dcbe449ef282f53f81aa563425bf
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-03-21 12:19:29 +0000

    prettier

--- cypress/e2e/datasets/list-page-searching.cy.ts
+++ cypress/e2e/datasets/list-page-searching.cy.ts
@@ -15,11 +15,10 @@ describe("Dataset list page searching", () => {
       cy.findAllByTestId("search-result")
         .should("have.length.least", 1)
         .each(($el) => {
-          cy.wrap($el).should("have.attr", "href").should("contain", "datasets/");
-          cy.wrap($el).should(
-            "contain.text",
-            "dataset")
-          
+          cy.wrap($el)
+            .should("have.attr", "href")
+            .should("contain", "datasets/");
+          cy.wrap($el).should("contain.text", "dataset");
         });
     });
   });
